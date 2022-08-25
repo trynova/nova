b51b43689299742b77de8dc55e7ed5d3793da466
Carter Snook <cartersnook04@gmail.com>
2022-08-25 15:44:51 -0500

    feat: basic tokenizer (#1)
    * feat: basic buggy parser
    
    * feat(tokenizer): add ident support
    
    * fix(tokenizer): infinite loop on idents

---
+++ tokenizer/examples/basic.rs
@@ -0,0 +1,14 @@
+use tokenizer::{TokenKind, TokenStream};
+
+pub fn main() {
+    let input = "2 == asdf 23 a ";
+    let mut stream = TokenStream::new(input.as_bytes());
+
+    loop {
+        let token = stream.next();
+        println!("{:?}", token);
+				if token.kind == TokenKind::Eof {
+            break;
+        }
+    }
+}
