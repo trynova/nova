5bb4b86efa5e1aebb75057afd17aa91b8842fdab
MierenManz <christiaanmens@gmail.com>
2022-08-29 22:00:14 +0200

    x

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,6 +4,6 @@ pub mod error;
 pub trait Reader: std::io::Read + std::io::Seek {}
 
 pub fn compile_module<R: Reader>(bytes: &mut R) -> Result<(), error::Error> {
-    let _module = decoder::Module::new(bytes);
+    let _module = decoder::Module::new(bytes)?;
     Ok(())
 }
