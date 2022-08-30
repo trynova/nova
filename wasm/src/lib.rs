084cbb69c222a1045abc6c4b269fa7b1571207a0
MierenManz <christiaanmens@gmail.com>
2022-08-30 22:21:19 +0200

    remove unneeded types

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,9 +2,7 @@ mod decoder;
 pub mod error;
 mod varint;
 
-pub trait Reader: std::io::Read + std::io::Seek {}
-
-pub fn compile_module<R: Reader>(bytes: &mut R) -> Result<(), error::Error> {
+pub fn compile_module<R: std::io::Read>(bytes: &mut R) -> Result<(), error::Error> {
     let _module = decoder::Module::new(bytes)?;
     todo!("Still need to add compiler and export generator");
 }
