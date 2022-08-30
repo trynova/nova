49bd3538a2429e911c527bd6d2ca333b03e1e750
MierenManz <christiaanmens@gmail.com>
2022-08-30 06:10:10 -0400

    add todo

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,5 +5,5 @@ pub trait Reader: std::io::Read + std::io::Seek {}
 
 pub fn compile_module<R: Reader>(bytes: &mut R) -> Result<(), error::Error> {
     let _module = decoder::Module::new(bytes)?;
-    Ok(())
+    todo!("Still need to add compiler and export generator");
 }
