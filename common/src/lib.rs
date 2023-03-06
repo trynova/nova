54e7cf2421c36f70c75f4a1d8090111fff1c0de1
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-03-06 08:45:15 -0500

    chore: fmt

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,14 +1,14 @@
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Span {
-  pub start: usize,
-  pub end: usize,
+    pub start: usize,
+    pub end: usize,
 }
 
 impl From<usize> for Span {
-  fn from(index: usize) -> Self {
-    Span {
-      start: index,
-      end: index,
+    fn from(index: usize) -> Self {
+        Span {
+            start: index,
+            end: index,
+        }
     }
-  }
 }
