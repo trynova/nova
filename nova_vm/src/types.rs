f6384070e71eb48b2562392acb80fea983464eea
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2023-10-20 23:20:42 +0300

    feat: Move ObjectHeapData to types/language/object/data

--- nova_vm/src/types.rs
+++ nova_vm/src/types.rs
@@ -1,6 +1,7 @@
 mod language;
 mod spec;
 
+pub(crate) use language::ObjectHeapData;
 pub(crate) use language::StringHeapData;
 pub use language::{Function, InternalMethods, Number, Object, PropertyKey, String, Value};
 pub use spec::{Base, PropertyDescriptor, Reference, ReferencedName};
--- nova_vm/src/types/language.rs
+++ nova_vm/src/types/language.rs
@@ -8,7 +8,7 @@ mod value;
 pub use bigint::BigInt;
 pub use function::Function;
 pub use number::Number;
-pub use object::{InternalMethods, Object, ObjectData, PropertyKey, PropertyStorage};
+pub use object::{InternalMethods, Object, ObjectHeapData, PropertyKey, PropertyStorage};
 pub(crate) use string::data::StringHeapData;
 pub use string::String;
 pub use value::Value;
