02cc5d36e979585f5ddb89daa01baff4d2a249eb
Andreu Botella <andreu@andreubotella.com>
2023-10-31 21:26:21 +0100

    feat(vm): Add conversions from integer types to `SmallInteger`
    This patch also makes `SmallInteger::from_i64_unchecked` a private
    method, to prevent misuse.

--- nova_vm/src/engine/small_integer.rs
+++ nova_vm/src/engine/small_integer.rs
@@ -28,7 +28,7 @@ impl SmallInteger {
         }
     }
 
-    pub fn from_i64_unchecked(value: i64) -> SmallInteger {
+    fn from_i64_unchecked(value: i64) -> SmallInteger {
         debug_assert!((Self::MIN_BIGINT..=Self::MAX_BIGINT).contains(&value));
         let bytes = i64::to_ne_bytes(value);
 
@@ -50,12 +50,63 @@ impl TryFrom<i64> for SmallInteger {
     type Error = ();
     fn try_from(value: i64) -> Result<Self, Self::Error> {
         if (Self::MIN_BIGINT..=Self::MAX_BIGINT).contains(&value) {
-            Ok(Self::from_i64_unchecked(value))
+            // SAFETY: We just checked the valid range.
+            Ok(unsafe { Self::from_i64_unchecked(value) })
         } else {
             Err(())
         }
     }
 }
+
+impl TryFrom<u64> for SmallInteger {
+    type Error = ();
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        if value <= (Self::MAX_BIGINT as u64) {
+            // SAFETY: Since Self::MIN_NUMBER is negative and Self::MAX_NUMBER
+            // positive, we know that value is within the valid range.
+            // Furthermore, since value is within a range that is valid for i64,
+            // we know that value is representable as i64.
+            Ok(unsafe { Self::from_i64_unchecked(value as i64) })
+        } else {
+            Err(())
+        }
+    }
+}
+
+macro_rules! from_numeric_type {
+    ($numtype:ty) => {
+        // Checking at compile-time that $numtype fully fits within the range.
+        const _: () = {
+            assert!(
+                <$numtype>::MIN as i64 >= SmallInteger::MIN_BIGINT,
+                concat!(
+                    stringify!($numtype),
+                    " is outside of the SmallInteger range (min)"
+                )
+            );
+            assert!(
+                <$numtype>::MAX as i64 <= SmallInteger::MAX_BIGINT,
+                concat!(
+                    stringify!($numtype),
+                    " is outside of the SmallInteger range (max)"
+                )
+            );
+        };
+        impl From<$numtype> for SmallInteger {
+            fn from(value: $numtype) -> Self {
+                // SAFETY: All values of $numtype fit in the {MIN,MAX}_BIGINT
+                // range, which we have verified at compile time above.
+                unsafe { Self::from_i64_unchecked(i64::from(value)) }
+            }
+        }
+    };
+}
+from_numeric_type!(u8);
+from_numeric_type!(i8);
+from_numeric_type!(u16);
+from_numeric_type!(i16);
+from_numeric_type!(u32);
+from_numeric_type!(i32);
 
 impl From<SmallInteger> for i64 {
     fn from(value: SmallInteger) -> Self {
