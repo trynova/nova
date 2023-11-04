00da7b364dfed2a482b317451a3316edb9090c75
Carter Snook <cartersnook04@gmail.com>
2023-11-04 14:41:01 -0500

    hopefully cleanup the indexing code

--- nova_vm/src/ecmascript/execution/realm.rs
+++ nova_vm/src/ecmascript/execution/realm.rs
@@ -55,7 +55,7 @@ impl<'ctx, 'host> Realm<'ctx, 'host> {
     pub fn create(agent: &mut Agent<'ctx, 'host>) -> RealmIdentifier<'ctx, 'host> {
         // TODO: implement spec
         let realm = Self {
-            global_env: GlobalEnvironmentIndex::from_u32_index(0),
+            global_env: GlobalEnvironmentIndex::from_u32(0),
             global_object: Object::Object(ObjectIndex::from_u32_index(0)),
             host_defined: None,
             intrinsics: Intrinsics::default(),
