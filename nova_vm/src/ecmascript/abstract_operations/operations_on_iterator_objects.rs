84537094eae8aefcf5b8259fb95c169d2f83dc98
Elias Sjögreen <elias@5monkeys.se>
2023-11-06 17:21:09 +0100

    chore: scaffold the rest of the abstract operations

--- nova_vm/src/ecmascript/abstract_operations/operations_on_iterator_objects.rs
+++ nova_vm/src/ecmascript/abstract_operations/operations_on_iterator_objects.rs
@@ -25,7 +25,7 @@ pub(crate) fn get_iterator_from_method(
 
     // 2. If iterator is not an Object, throw a TypeError exception.
     let Ok(iterator) = to_object(agent, iterator) else {
-        return Err(agent.throw_exception(ExceptionType::TypeError, "Iterator is not object"));
+        return Err(agent.throw_exception(ExceptionType::TypeError, "Iterator is not an object"));
     };
 
     // 3. Let nextMethod be ? Get(iterator, "next").
@@ -98,4 +98,141 @@ pub(crate) fn iterator_value(agent: &mut Agent, iter_result: Object) -> JsResult
     // 1. Return ? Get(iterResult, "value").
     let property = PropertyKey::from_str(&mut agent.heap, "value");
     get(agent, iter_result, property)
+}
+
+/// [7.4.7 IteratorStep ( iteratorRecord )](https://tc39.es/ecma262/#sec-iteratorstep)
+///
+/// The abstract operation IteratorStep takes argument iteratorRecord (an
+/// Iterator Record) and returns either a normal completion containing either
+/// an Object or false, or a throw completion. It requests the next value from
+/// iteratorRecord.\[\[Iterator\]\] by calling iteratorRecord.\[\[NextMethod\]\]
+/// and returns either false indicating that the iterator has reached its end
+/// or the IteratorResult object if a next value is available.
+pub(crate) fn iterator_step(agent: &mut Agent, iterator_record: Object) -> JsResult<Value> {
+    // 1. Let result be ? IteratorNext(iteratorRecord).
+    let result = iterator_next(agent, iterator_record, None)?;
+
+    // 2. Let done be ? IteratorComplete(result).
+    let done = iterator_complete(agent, result)?;
+
+    // 3. If done is true, return false.
+    if done.is_true() {
+        return Ok(Value::Boolean(false));
+    }
+
+    // 4. Return result.
+    Ok(result.into())
+}
+
+/// [7.4.8 IteratorClose ( iteratorRecord, completion )](https://tc39.es/ecma262/#sec-iteratorclose)
+///
+/// The abstract operation IteratorClose takes arguments iteratorRecord (an
+/// Iterator Record) and completion (a Completion Record) and returns a
+/// Completion Record. It is used to notify an iterator that it should perform
+/// any actions it would normally perform when it has reached its completed state.
+pub(crate) fn iterator_close(
+    agent: &mut Agent,
+    iterator_record: Object,
+    completion: Value,
+) -> JsResult<Value> {
+    // 1. Assert: iteratorRecord.[[Iterator]] is an Object.
+    // 2. Let iterator be iteratorRecord.[[Iterator]].
+    // 3. Let innerResult be Completion(GetMethod(iterator, "return")).
+    // 4. If innerResult.[[Type]] is normal, then
+    // a. Let return be innerResult.[[Value]].
+    // b. If return is undefined, return ? completion.
+    // c. Set innerResult to Completion(Call(return, iterator)).
+    // 5. If completion.[[Type]] is throw, return ? completion.
+    // 6. If innerResult.[[Type]] is throw, return ? innerResult.
+    // 7. If innerResult.[[Value]] is not an Object, throw a TypeError exception.
+    // 8. Return ? completion.
+    todo!()
+}
+
+/// [7.4.9 IfAbruptCloseIterator ( value, iteratorRecord )](https://tc39.es/ecma262/#sec-ifabruptcloseiterator)
+///
+/// IfAbruptCloseIterator is a shorthand for a sequence of algorithm steps that use an Iterator Record.
+pub(crate) fn if_abrupt_close_iterator(
+    agent: &mut Agent,
+    value: Value,
+    iterator_record: Object,
+) -> JsResult<Value> {
+    // 1. Assert: value is a Completion Record.
+    // 2. If value is an abrupt completion, return ? IteratorClose(iteratorRecord, value).
+    // 3. Else, set value to value.[[Value]].
+    todo!()
+}
+
+/// [7.4.10 AsyncIteratorClose ( iteratorRecord, completion )](https://tc39.es/ecma262/#sec-asynciteratorclose)
+///
+/// The abstract operation AsyncIteratorClose takes arguments iteratorRecord
+/// (an Iterator Record) and completion (a Completion Record) and returns a
+/// Completion Record. It is used to notify an async iterator that it should
+/// perform any actions it would normally perform when it has reached its
+/// completed state.
+pub(crate) fn async_iterator_close(
+    agent: &mut Agent,
+    iterator_record: Object,
+    completion: Value,
+) -> JsResult<Value> {
+    // 1. Assert: iteratorRecord.[[Iterator]] is an Object.
+    // 2. Let iterator be iteratorRecord.[[Iterator]].
+    // 3. Let innerResult be Completion(GetMethod(iterator, "return")).
+    // 4. If innerResult.[[Type]] is normal, then
+    // a. Let return be innerResult.[[Value]].
+    // b. If return is undefined, return ? completion.
+    // c. Set innerResult to Completion(Call(return, iterator)).
+    // d. If innerResult.[[Type]] is normal, set innerResult to Completion(Await(innerResult.[[Value]])).
+    // 5. If completion.[[Type]] is throw, return ? completion.
+    // 6. If innerResult.[[Type]] is throw, return ? innerResult.
+    // 7. If innerResult.[[Value]] is not an Object, throw a TypeError exception.
+    // 8. Return ? completion.
+    todo!()
+}
+
+/// [7.4.11 CreateIterResultObject ( value, done )](https://tc39.es/ecma262/#sec-createiterresultobject)
+///
+/// The abstract operation CreateIterResultObject takes arguments value (an
+/// ECMAScript language value) and done (a Boolean) and returns an Object that
+/// conforms to the IteratorResult interface. It creates an object that conforms
+/// to the IteratorResult interface.
+pub(crate) fn create_iter_result_object(agent: &mut Agent, value: Value, done: bool) -> Value {
+    // 1. Let obj be OrdinaryObjectCreate(%Object.prototype%).
+    // 2. Perform ! CreateDataPropertyOrThrow(obj, "value", value).
+    // 3. Perform ! CreateDataPropertyOrThrow(obj, "done", done).
+    // 4. Return obj.
+    todo!()
+}
+
+/// [7.4.12 CreateListIteratorRecord ( list )](https://tc39.es/ecma262/#sec-createlistiteratorRecord)
+///
+/// The abstract operation CreateListIteratorRecord takes argument list (a List
+/// of ECMAScript language values) and returns an Iterator Record. It creates
+/// an Iterator (27.1.1.2) object record whose next method returns the
+/// successive elements of list.
+pub(crate) fn create_list_iterator_record(agent: &mut Agent, list: Vec<Value>) -> JsResult<Value> {
+    // 1. Let closure be a new Abstract Closure with no parameters that captures list and performs the following steps when called:
+    // a. For each element E of list, do
+    // i. Perform ? GeneratorYield(CreateIterResultObject(E, false)).
+    // b. Return NormalCompletion(undefined).
+    // 2. Let iterator be CreateIteratorFromClosure(closure, empty, %IteratorPrototype%).
+    // 3. Return the Iterator Record { [[Iterator]]: iterator, [[NextMethod]]: %GeneratorFunction.prototype.prototype.next%, [[Done]]: false }.
+    todo!()
+}
+
+/// [7.4.13 IteratorToList ( iteratorRecord )](https://tc39.es/ecma262/#sec-iteratortolist)
+///
+/// The abstract operation IteratorToList takes argument iteratorRecord (an
+/// Iterator Record) and returns either a normal completion containing a List
+/// of ECMAScript language values or a throw completion.
+pub(crate) fn iterator_to_list(agent: &mut Agent, iterator_record: Object) -> JsResult<Vec<Value>> {
+    // 1. Let values be a new empty List.
+    // 2. Let next be true.
+    // 3. Repeat, while next is not false,
+    // a. Set next to ? IteratorStep(iteratorRecord).
+    // b. If next is not false, then
+    // i. Let nextValue be ? IteratorValue(next).
+    // ii. Append nextValue to values.
+    // 4. Return values.
+    todo!()
 }
