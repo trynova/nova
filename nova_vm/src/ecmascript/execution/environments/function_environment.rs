2e55607d75de1516af811bdbccbcd3a82b743520
Carter Snook <cartersnook04@gmail.com>
2023-11-24 12:12:27 -0600

    feat(execution): implement more environments logic (#64)

--- nova_vm/src/ecmascript/execution/environments/function_environment.rs
+++ nova_vm/src/ecmascript/execution/environments/function_environment.rs
@@ -1,36 +1,14 @@
-use std::{collections::HashMap, marker::PhantomData, num::NonZeroU32};
-
-use super::{declarative_environment::Binding, EnvironmentIndex};
+use super::{DeclarativeEnvironment, OuterEnv};
 use crate::{
-    ecmascript::types::{Object, String, Value},
-    heap::indexes::FunctionIndex,
+    ecmascript::{
+        builtins::ECMAScriptFunction,
+        execution::{agent::ExceptionType, Agent, JsResult},
+        types::{Function, Object, Value},
+    },
+    heap::{indexes::FunctionIndex, GetHeapData},
 };
 
-#[derive(Debug, Clone, Copy)]
-pub struct FunctionEnvironmentIndex(NonZeroU32, PhantomData<FunctionEnvironment>);
-
-impl FunctionEnvironmentIndex {
-    pub const fn from_u32_index(value: u32) -> Self {
-        assert!(value != u32::MAX);
-        // SAFETY: Number is not max value and will not overflow to zero.
-        // This check is done manually to allow const context.
-        Self(unsafe { NonZeroU32::new_unchecked(value + 1) }, PhantomData)
-    }
-
-    pub const fn from_usize_index(value: usize) -> Self {
-        debug_assert!(value < u32::MAX as usize);
-        Self(
-            unsafe { NonZeroU32::new_unchecked(value as u32 + 1) },
-            PhantomData,
-        )
-    }
-
-    pub const fn into_index(self) -> usize {
-        self.0.get() as usize - 1
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum ThisBindingStatus {
     /// Function is an ArrowFunction and does not have a local `this` value.
     Lexical,
@@ -40,32 +18,145 @@ pub enum ThisBindingStatus {
     Uninitialized,
 }
 
-/// 9.1.1.3 Function Environment Records
-/// https://tc39.es/ecma262/#sec-function-environment-records
+/// ### [9.1.1.3 Function Environment Records](https://tc39.es/ecma262/#sec-function-environment-records)
+///
+/// A Function Environment Record is a Declarative Environment Record that is
+/// used to represent the top-level scope of a function and, if the function is
+/// not an ArrowFunction, provides a this binding. If a function is not an
+/// ArrowFunction function and references super, its Function Environment Record
+/// also contains the state that is used to perform super method invocations
+/// from within the function.
 #[derive(Debug)]
 pub struct FunctionEnvironment {
-    /// [[ThisValue]]
-    this_value: Value,
+    /// ### \[\[ThisValue\]\]
+    ///
+    /// This is the this value used for this invocation of the function.
+    this_value: Option<Value>,
 
-    /// [[ThisBindingStatus]]
+    /// ### \[\[ThisBindingStatus\]\]
+    ///
+    /// If the value is LEXICAL, this is an ArrowFunction and does not have a
+    /// local this value.
     this_binding_status: ThisBindingStatus,
 
-    /// [[FunctionObject]]
-    function_object: FunctionIndex,
-
-    /// [[NewTarget]]
+    /// ### \[\[FunctionObject\]\]
     ///
-    /// If this FunctionEnvironment was created with a [[Construct]] internal method,
-    /// this is the value of the _newTarget_ parameter. Otherwise, its value is **undefined**
-    /// (implementation wise here None).
+    /// The function object whose invocation caused this Environment Record to
+    /// be created.
+    function_object: Function,
+
+    /// ### \[\[NewTarget\]\]
+    ///
+    /// If this Environment Record was created by the \[\[Construct\]\] internal
+    /// method, \[\[NewTarget\]\] is the value of the \[\[Construct\]\]
+    /// newTarget parameter. Otherwise, its value is undefined.
     new_target: Option<Object>,
 
-    /// [[OuterEnv]]
-    outer_env: Option<EnvironmentIndex>,
+    /// Function Environment Records support all of the Declarative Environment
+    /// Record methods listed in Table 16 and share the same specifications for
+    /// all of those methods except for HasThisBinding and HasSuperBinding.
+    declarative_environment: DeclarativeEnvironment,
+}
 
-    /// Per https://tc39.es/ecma262/#sec-the-environment-record-type-hierarchy:
-    /// > A _Function Environment Record_ is a _Declarative Environment Record_ [...]
+impl std::ops::Deref for FunctionEnvironment {
+    type Target = DeclarativeEnvironment;
+    fn deref(&self) -> &Self::Target {
+        &self.declarative_environment
+    }
+}
+
+impl FunctionEnvironment {
+    /// ### [9.1.2.4 NewFunctionEnvironment ( F, newTarget )](https://tc39.es/ecma262/#sec-newfunctionenvironment)
     ///
-    /// The Declaration Environment Record is inlined here.
-    declarative_environment: HashMap<String, Binding>,
+    /// The abstract operation NewFunctionEnvironment takes arguments F (an
+    /// ECMAScript function object) and newTarget (an Object or undefined) and
+    /// returns a Function Environment Record.
+    pub(crate) fn new(
+        agent: &Agent,
+        function_object: Function,
+        new_target: Option<Object>,
+    ) -> FunctionEnvironment {
+        // 1. Let env be a new Function Environment Record containing no bindings.
+        FunctionEnvironment {
+            this_value: None,
+
+            // 2. Set env.[[FunctionObject]] to F.
+            function_object,
+
+            // 3. If F.[[ThisMode]] is LEXICAL, set env.[[ThisBindingStatus]] to LEXICAL.
+            // 4. Else, set env.[[ThisBindingStatus]] to UNINITIALIZED.
+            this_binding_status: ThisBindingStatus::Uninitialized,
+
+            // 5. Set env.[[NewTarget]] to newTarget.
+            new_target,
+
+            // 6. Set env.[[OuterEnv]] to F.[[Environment]].
+            declarative_environment: todo!(),
+        }
+        // 7. Return env.
+    }
+
+    /// ### [9.1.1.3.1 BindThisValue ( V )](https://tc39.es/ecma262/#sec-bindthisvalue)
+    ///
+    /// The BindThisValue concrete method of a Function Environment Record envRec takes argument V (an ECMAScript language value) and returns either a normal completion containing an ECMAScript language value or a throw completion. It performs the following steps when called:
+    pub(crate) fn bind_this_value(&mut self, agent: &mut Agent, value: Value) -> JsResult<Value> {
+        // 1. Assert: envRec.[[ThisBindingStatus]] is not LEXICAL.
+        debug_assert!(self.this_binding_status != ThisBindingStatus::Lexical);
+
+        // 2. If envRec.[[ThisBindingStatus]] is INITIALIZED, throw a
+        // ReferenceError exception.
+        if self.this_binding_status == ThisBindingStatus::Initialized {
+            return Err(agent.throw_exception(
+                ExceptionType::ReferenceError,
+                "Identifier is already initialized.",
+            ));
+        }
+
+        // 3. Set envRec.[[ThisValue]] to V.
+        self.this_value = Some(value);
+
+        // 4. Set envRec.[[ThisBindingStatus]] to INITIALIZED.
+        self.this_binding_status = ThisBindingStatus::Initialized;
+
+        // 5. Return V.
+        Ok(value)
+    }
+
+    /// ### [9.1.1.3.2 HasThisBinding ( )](https://tc39.es/ecma262/#sec-function-environment-records-hasthisbinding)
+    ///
+    /// The HasThisBinding concrete method of a Function Environment Record
+    /// envRec takes no arguments and returns a Boolean.
+    pub(crate) fn has_this_binding(&self) -> bool {
+        // 1. If envRec.[[ThisBindingStatus]] is LEXICAL, return false;
+        // otherwise, return true.
+        self.this_binding_status != ThisBindingStatus::Lexical
+    }
+
+    /// ### [9.1.1.3.3 HasSuperBinding ( )](https://tc39.es/ecma262/#sec-function-environment-records-hassuperbinding)
+    ///
+    /// The HasSuperBinding concrete method of a Function Environment Record
+    /// envRec takes no arguments and returns a Boolean.
+    pub(crate) fn has_super_binding(&self, agent: &Agent) -> bool {
+        // 1. If envRec.[[ThisBindingStatus]] is LEXICAL, return false.
+        if self.this_binding_status == ThisBindingStatus::Lexical {
+            return false;
+        }
+
+        // TODO: 2. If envRec.[[FunctionObject]].[[HomeObject]] is undefined, return false; otherwise, return true.
+        todo!("Finish this")
+    }
+
+    /// ### [9.1.1.3.5 GetSuperBase ( )](https://tc39.es/ecma262/#sec-getsuperbase)
+    ///
+    /// The GetSuperBase concrete method of a Function Environment Record envRec
+    /// takes no arguments and returns either a normal completion containing
+    /// either an Object, null, or undefined, or a throw completion.
+    pub(crate) fn get_super_base(&self) -> Value {
+        todo!("Finish this")
+
+        // 1. Let home be envRec.[[FunctionObject]].[[HomeObject]].
+        // 2. If home is undefined, return undefined.
+        // 3. Assert: home is an Object.
+        // 4. Return ? home.[[GetPrototypeOf]]().
+    }
 }
--- nova_vm/src/ecmascript/execution/environments/object_environment.rs
+++ nova_vm/src/ecmascript/execution/environments/object_environment.rs
@@ -1,42 +1,58 @@
-use std::{marker::PhantomData, num::NonZeroU32};
-
-use super::EnvironmentIndex;
+use super::OuterEnv;
 use crate::ecmascript::types::Object;
 
-#[derive(Debug, Clone, Copy)]
-pub struct ObjectEnvironmentIndex(NonZeroU32, PhantomData<ObjectEnvironment>);
-
-impl ObjectEnvironmentIndex {
-    pub const fn from_u32_index(value: u32) -> Self {
-        assert!(value != u32::MAX);
-        // SAFETY: Number is not max value and will not overflow to zero.
-        // This check is done manually to allow const context.
-        Self(unsafe { NonZeroU32::new_unchecked(value + 1) }, PhantomData)
-    }
-
-    pub const fn from_usize_index(value: usize) -> Self {
-        debug_assert!(value < u32::MAX as usize);
-        Self(
-            unsafe { NonZeroU32::new_unchecked(value as u32 + 1) },
-            PhantomData,
-        )
-    }
-
-    pub const fn into_index(self) -> usize {
-        self.0.get() as usize - 1
-    }
-}
-
-/// 9.1.1.2 Object Environment Records
-/// https://tc39.es/ecma262/#sec-object-environment-records
+/// ### [9.1.1.2 Object Environment Records](https://tc39.es/ecma262/#sec-object-environment-records)
+///
+/// Each Object Environment Record is associated with an object called its
+/// binding object. An Object Environment Record binds the set of string
+/// identifier names that directly correspond to the property names of its
+/// binding object. Property keys that are not strings in the form of an
+/// IdentifierName are not included in the set of bound identifiers. Both own
+/// and inherited properties are included in the set regardless of the setting
+/// of their \[\[Enumerable\]\] attribute. Because properties can be dynamically
+/// added and deleted from objects, the set of identifiers bound by an Object
+/// Environment Record may potentially change as a side-effect of any operation
+/// that adds or deletes properties. Any bindings that are created as a result
+/// of such a side-effect are considered to be a mutable binding even if the
+/// Writable attribute of the corresponding property is false. Immutable
+/// bindings do not exist for Object Environment Records.
 #[derive(Debug)]
 pub struct ObjectEnvironment {
-    /// [[BindingObject]]
+    /// ### \[\[BindingObject\]\]
+    ///
+    /// The binding object of this Environment Record.
     binding_object: Object,
 
-    /// [[IsWithEnvironment]]
+    /// ### \[\[IsWithEnvironment\]\]
+    ///
+    /// Indicates whether this Environment Record is created for a with
+    /// statement.
     is_with_environment: bool,
 
-    /// [[OuterEnv]]
-    outer_env: Option<EnvironmentIndex>,
+    /// ### \[\[OuterEnv\]\]
+    ///
+    /// See [OuterEnv].
+    outer_env: OuterEnv,
+}
+
+impl ObjectEnvironment {
+    /// ### [9.1.2.3 NewObjectEnvironment ( O, W, E )](https://tc39.es/ecma262/#sec-newobjectenvironmenthttps://tc39.es/ecma262/#sec-newobjectenvironment)
+    ///
+    /// The abstract operation NewObjectEnvironment takes arguments O (an Object), W (a Boolean), and E (an Environment Record or null) and returns an Object Environment Record. It performs the following steps when called:
+    pub(crate) fn new(
+        binding_object: Object,
+        is_with_environment: bool,
+        outer_env: OuterEnv,
+    ) -> ObjectEnvironment {
+        // 1. Let env be a new Object Environment Record.
+        ObjectEnvironment {
+            // 2. Set env.[[BindingObject]] to O.
+            binding_object,
+            // 3. Set env.[[IsWithEnvironment]] to W.
+            is_with_environment,
+            // 4. Set env.[[OuterEnv]] to E.
+            outer_env,
+        }
+        // 5. Return env.
+    }
 }
