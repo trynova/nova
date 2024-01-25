8dd0e92500cf896df7b479de6fc2f019cf648ca1
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2024-01-25 21:20:06 +0200

    fmt

--- nova_vm/src/ecmascript/types/language/value.rs
+++ nova_vm/src/ecmascript/types/language/value.rs
@@ -175,8 +175,10 @@ impl Value {
     }
 
     pub fn is_function(self) -> bool {
-        matches!(self, Value::BoundFunction(_) | Value::BuiltinFunction(_)
-            | Value::ECMAScriptFunction(_))
+        matches!(
+            self,
+            Value::BoundFunction(_) | Value::BuiltinFunction(_) | Value::ECMAScriptFunction(_)
+        )
     }
 
     pub fn is_string(self) -> bool {
