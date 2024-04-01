414a21988ea4e10872d491cf35ae0d080b6a6568
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2024-04-01 23:57:03 +0300

    fix(tests): Leftover comment

--- nova_vm/tests/sources/objectPrototype.test.js
+++ nova_vm/tests/sources/objectPrototype.test.js
@@ -10,7 +10,6 @@ const objectOfNullObject = Object.create(nullObject);
 if (!objectOfNullObject) {
     throw new Error("'Object.create(nullObject)' produced nullish output");
 }
-// TODO: Object.create handling is mistaken
 if (Object.getPrototypeOf(objectOfNullObject) !== nullObject) {
     throw new Error("'Object.create(nullObject)' did not produce object with nullObject prototype");
 }
