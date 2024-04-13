c5ebbc1203716f22d15b49240c50c132e7a7e489
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2024-04-13 17:17:05 +0300

    fix missing file

---
+++ nova_vm/src/ecmascript/builtins/text_processing/string_objects/string_iterator_objects.rs
@@ -0,0 +1,44 @@
+use crate::{
+    ecmascript::{
+        builders::ordinary_object_builder::OrdinaryObjectBuilder,
+        builtins::{ArgumentsList, Builtin},
+        execution::{Agent, JsResult, RealmIdentifier},
+        types::{String, Value, BUILTIN_STRING_MEMORY},
+    },
+    heap::WellKnownSymbolIndexes,
+};
+
+pub(crate) struct StringIteratorPrototype;
+
+struct StringIteratorPrototypeNext;
+impl Builtin for StringIteratorPrototypeNext {
+    const NAME: String = BUILTIN_STRING_MEMORY.next;
+
+    const LENGTH: u8 = 0;
+
+    const BEHAVIOUR: crate::ecmascript::builtins::Behaviour =
+        crate::ecmascript::builtins::Behaviour::Regular(StringIteratorPrototype::next);
+}
+
+impl StringIteratorPrototype {
+    fn next(_agent: &mut Agent, _this_value: Value, _arguments: ArgumentsList) -> JsResult<Value> {
+        todo!();
+    }
+
+    pub(crate) fn create_intrinsic(agent: &mut Agent, realm: RealmIdentifier) {
+        let intrinsics = agent.get_realm(realm).intrinsics();
+        let this = intrinsics.string_iterator_prototype();
+
+        OrdinaryObjectBuilder::new_intrinsic_object(agent, realm, this)
+            .with_property_capacity(2)
+            .with_builtin_function_property::<StringIteratorPrototypeNext>()
+            .with_property(|builder| {
+                builder
+                    .with_key(WellKnownSymbolIndexes::ToStringTag.into())
+                    .with_value_readonly(BUILTIN_STRING_MEMORY.String_Iterator.into_value())
+                    .with_enumerable(false)
+                    .build()
+            })
+            .build();
+    }
+}
