636cb484a98795c6031421970daad7feb0fd3773
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2024-06-23 10:09:24 +0300

    feat(ecmascript): isFinite and isNaN

--- nova_vm/src/ecmascript/builtins/global_object.rs
+++ nova_vm/src/ecmascript/builtins/global_object.rs
@@ -1,5 +1,6 @@
 use crate::{
     ecmascript::{
+        abstract_operations::type_conversion::to_number,
         builders::builtin_function_builder::BuiltinFunctionBuilder,
         execution::{Agent, JsResult, RealmIdentifier},
         types::{String, Value, BUILTIN_STRING_MEMORY},
@@ -115,11 +116,34 @@ impl GlobalObject {
     fn eval(_agent: &mut Agent, _this_value: Value, _: ArgumentsList) -> JsResult<Value> {
         todo!()
     }
-    fn is_finite(_agent: &mut Agent, _this_value: Value, _: ArgumentsList) -> JsResult<Value> {
-        todo!()
+
+    /// ### [19.2.2 isFinite ( number )](https://tc39.es/ecma262/#sec-isfinite-number)
+    ///
+    /// This function is the %isFinite% intrinsic object.
+
+    fn is_finite(agent: &mut Agent, _: Value, arguments: ArgumentsList) -> JsResult<Value> {
+        let number = arguments.get(0);
+        // 1. Let num be ? ToNumber(number).
+        let num = to_number(agent, number)?;
+        // 2. If num is not finite, return false.
+        // 3. Otherwise, return true.
+        Ok(num.is_finite(agent).into())
     }
-    fn is_nan(_agent: &mut Agent, _this_value: Value, _: ArgumentsList) -> JsResult<Value> {
-        todo!()
+
+    /// ### [19.2.3 isNaN ( number )](https://tc39.es/ecma262/#sec-isnan-number)
+    ///
+    /// This function is the %isNaN% intrinsic object.
+    ///
+    /// > NOTE: A reliable way for ECMAScript code to test if a value X is NaN
+    /// > is an expression of the form X !== X. The result will be true if and
+    /// > only if X is NaN.
+    fn is_nan(agent: &mut Agent, _: Value, arguments: ArgumentsList) -> JsResult<Value> {
+        let number = arguments.get(0);
+        // 1. Let num be ? ToNumber(number).
+        let num = to_number(agent, number)?;
+        // 2. If num is NaN, return true.
+        // 3. Otherwise, return false.
+        Ok(num.is_nan(agent).into())
     }
     fn parse_float(_agent: &mut Agent, _this_value: Value, _: ArgumentsList) -> JsResult<Value> {
         todo!()
