fd6e065fd4b4b620e2d60db1779ea98c4ffb7acb
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2024-07-08 21:21:37 +0300

    chore(nova_vm): Add comments to builtin strings

--- nova_vm/build.rs
+++ nova_vm/build.rs
@@ -53,6 +53,9 @@ fn gen_builtin_strings() -> io::Result<Vec<u8>> {
     }
     output.push_str("];\n\n#[allow(non_snake_case)]\npub struct BuiltinStrings {\n");
     for string in &strings {
+        output.push_str("    /// ```js\n");
+        output.push_str(&format!("    /// \"{}\"\n", string));
+        output.push_str("    /// ```\n");
         output.push_str("    pub r#");
         output.push_str(&replace_invalid_key_characters(string));
         output.push_str(": String,\n");
