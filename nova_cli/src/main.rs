trynova/nova

commit 83758ad7a16632c526a4d7dac9587b07c57772f1

feat(ecmascript, cli): Implement promise jobs (#303)
The last piece of the basic Promise implementation, this patch
implements promise jobs, as well as the `HostEnqueuePromiseJob` host
hook that gives them to the embedder to enqueue and later run.

Although the spec makes a difference between various kinds of jobs
(e.g. promise vs timeout vs `FinalizationRegistry` cleanup jobs), and
implementations typically treat them differently for scheduling, the
spec does not require that they are stored in separate queues. We
therefore have a single embedder-facing `Job` type, so jobs of
different kinds might be stored in the same queue. This type wraps an
enum that disambiguates between the different internal job structs,
each with a `run()` implementation.

This patch also adds the `HostPromiseRejectionTracker` hook, meant to
enable hosts to track promises that have rejected without catch
handlers (e.g. for the `unhandledrejection` HTML event).

nova_cli/src/main.rs
@@ -2,9 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::{cell::RefCell, fmt::Debug};
+
 use clap::{Parser as ClapParser, Subcommand};
 use nova_vm::ecmascript::{
-    execution::{agent::Options, initialize_host_defined_realm, Agent, DefaultHostHooks, Realm},
+    execution::{
+        agent::{HostHooks, Job, Options},
+        initialize_host_defined_realm, Agent, Realm,
+    },
     scripts_and_modules::script::{parse_script, script_evaluation},
     types::{Object, Value},
 };
@@ -40,6 +45,32 @@ enum Command {
     },
 }
 
+#[derive(Default)]
+struct CliHostHooks {
+    promise_job_queue: RefCell<Vec<Job>>,
+}
+
+// RefCell doesn't implement Debug
+impl Debug for CliHostHooks {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CliHostHooks")
+            //.field("promise_job_queue", &*self.promise_job_queue.borrow())
+            .finish()
+    }
+}
+
+impl CliHostHooks {
+    fn pop_promise_job(&self) -> Option<Job> {
+        self.promise_job_queue.borrow_mut().pop()
+    }
+}
+
+impl HostHooks for CliHostHooks {
+    fn enqueue_promise_job(&self, job: Job) {
+        self.promise_job_queue.borrow_mut().push(job);
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
 
@@ -59,12 +90,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         Command::Eval { verbose, paths } => {
             let allocator = Default::default();
 
+            let host_hooks: &CliHostHooks = &*Box::leak(Box::default());
             let mut agent = Agent::new(
                 Options {
                     disable_gc: false,
                     print_internals: verbose,
                 },
-                &DefaultHostHooks,
+                host_hooks,
             );
             {
                 let create_global_object: Option<fn(&mut Realm) -> Object> = None;
@@ -92,6 +124,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 final_result = script_evaluation(&mut agent, script);
                 if final_result.is_err() {
                     break;
+                }
+            }
+
+            if final_result.is_ok() {
+                while let Some(job) = host_hooks.pop_promise_job() {
+                    if let Err(err) = job.run(&mut agent) {
+                        final_result = Err(err);
+                        break;
+                    }
                 }
             }
 

nova_vm/src/ecmascript/builtins/control_abstraction_objects/promise_objects/promise_abstract_operations.rs
@@ -3,5 +3,6 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 pub(crate) mod promise_capability_records;
+pub(crate) mod promise_jobs;
 pub(crate) mod promise_reaction_records;
 pub(crate) mod promise_resolving_functions;

nova_vm/src/ecmascript/builtins/control_abstraction_objects/promise_objects/promise_abstract_operations/promise_capability_records.rs
@@ -12,13 +12,15 @@ use crate::{
             Promise,
         },
         execution::{
-            agent::{ExceptionType, JsError},
+            agent::{ExceptionType, JsError, PromiseRejectionTrackerOperation},
             Agent, JsResult,
         },
         types::{Function, IntoValue, Object, Value, BUILTIN_STRING_MEMORY},
     },
     heap::{CompactionLists, CreateHeapData, HeapMarkAndSweep, WorkQueues},
 };
+
+use super::promise_jobs::new_promise_resolve_thenable_job;
 
 /// A promise capability encapsulates a promise, adding methods that are capable
 /// of resolving or rejecting that promise.
@@ -107,9 +109,9 @@ impl PromiseCapability {
         };
 
         // 7. If promise.[[PromiseIsHandled]] is false, perform HostPromiseRejectionTracker(promise, "reject").
-        //agent
-        //    .host_hooks
-        //    .promise_rejection_tracker(self.promise, PromiseRejectionTrackerOperation::Reject);
+        agent
+            .host_hooks
+            .promise_rejection_tracker(self.promise, PromiseRejectionTrackerOperation::Reject);
 
         // 8. Perform TriggerPromiseReactions(reactions, reason)
         if let Some(reactions) = reactions {
@@ -175,7 +177,7 @@ impl PromiseCapability {
 
         // 12. If IsCallable(thenAction) is false, then
         // TODO: Callable proxies
-        let Ok(_then_action) = Function::try_from(then_action) else {
+        let Ok(then_action) = Function::try_from(then_action) else {
             // a. Perform FulfillPromise(promise, resolution).
             self.internal_fulfill(agent, resolution.into_value());
             // b. Return undefined.
@@ -183,11 +185,12 @@ impl PromiseCapability {
         };
 
         // 13. Let thenJobCallback be HostMakeJobCallback(thenAction).
-        // TODO: HostMakeJobCallback
+        // TODO: Add the HostMakeJobCallback host hook. Leaving it for later, since in
+        // implementations other than browsers, [[HostDefine]] must be EMPTY.
         // 14. Let job be NewPromiseResolveThenableJob(promise, resolution, thenJobCallback).
-        //let job = new_promise_resolve_thenable_job(agent, self.promise, resolution, then_action);
+        let job = new_promise_resolve_thenable_job(agent, self.promise, resolution, then_action);
         // 15. Perform HostEnqueuePromiseJob(job.[[Job]], job.[[Realm]]).
-        //agent.host_hooks.enqueue_promise_job(job);
+        agent.host_hooks.enqueue_promise_job(job);
         // 16. Return undefined.
     }
 

nova_vm/src/ecmascript/builtins/control_abstraction_objects/promise_objects/promise_abstract_operations/promise_jobs.rs
@@ -1,0 +1,170 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! ## [27.2.2 Promise Jobs](https://tc39.es/ecma262/#sec-promise-jobs)
+
+use crate::{ecmascript::{
+    abstract_operations::operations_on_objects::{call_function, get_function_realm}, builtins::{control_abstraction_objects::promise_objects::promise_abstract_operations::promise_capability_records::PromiseCapability, promise::Promise, ArgumentsList}, execution::{agent::{InnerJob, Job, JsError}, Agent, JsResult}, types::{Function, IntoValue, Object, Value}
+}, heap::CreateHeapData};
+
+use super::{
+    promise_reaction_records::{PromiseReaction, PromiseReactionHandler, PromiseReactionType},
+    promise_resolving_functions::{PromiseResolvingFunctionHeapData, PromiseResolvingFunctionType},
+};
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct PromiseResolveThenableJob {
+    promise_to_resolve: Promise,
+    thenable: Object,
+    then: Function,
+}
+impl PromiseResolveThenableJob {
+    pub(crate) fn run(self, agent: &mut Agent) -> JsResult<()> {
+        // The following are substeps of point 1 in NewPromiseResolveThenableJob.
+        // a. Let resolvingFunctions be CreateResolvingFunctions(promiseToResolve).
+        let promise_capability = PromiseCapability::from_promise(self.promise_to_resolve, false);
+        let resolve_function = agent
+            .heap
+            .create(PromiseResolvingFunctionHeapData {
+                object_index: None,
+                promise_capability,
+                resolve_type: PromiseResolvingFunctionType::Resolve,
+            })
+            .into_value();
+        let reject_function = agent
+            .heap
+            .create(PromiseResolvingFunctionHeapData {
+                object_index: None,
+                promise_capability,
+                resolve_type: PromiseResolvingFunctionType::Reject,
+            })
+            .into_value();
+
+        // b. Let thenCallResult be Completion(HostCallJobCallback(then, thenable, « resolvingFunctions.[[Resolve]], resolvingFunctions.[[Reject]] »)).
+        // TODO: Add the HostCallJobCallback host hook. For now we're using its default
+        // implementation, which is calling the thenable, since only browsers should use a different
+        // implementation.
+        let then_call_result = call_function(
+            agent,
+            self.then,
+            self.thenable.into_value(),
+            Some(ArgumentsList(&[resolve_function, reject_function])),
+        );
+
+        // c. If thenCallResult is an abrupt completion, then
+        if let Err(err) = then_call_result {
+            // i. Return ? Call(resolvingFunctions.[[Reject]], undefined, « thenCallResult.[[Value]] »).
+            promise_capability.reject(agent, err.value());
+        }
+        // d. Return ? thenCallResult.
+        Ok(())
+    }
+}
+
+/// ### [27.2.2.2 NewPromiseResolveThenableJob ( promiseToResolve, thenable, then )](https://tc39.es/ecma262/#sec-newpromiseresolvethenablejob)
+pub(crate) fn new_promise_resolve_thenable_job(
+    agent: &mut Agent,
+    promise_to_resolve: Promise,
+    thenable: Object,
+    then: Function,
+) -> Job {
+    // 2. Let getThenRealmResult be Completion(GetFunctionRealm(then.[[Callback]])).
+    // 5. NOTE: thenRealm is never null. When then.[[Callback]] is a revoked Proxy and no code runs, thenRealm is used to create error objects.
+    let then_realm = match get_function_realm(agent, then) {
+        // 3. If getThenRealmResult is a normal completion, let thenRealm be getThenRealmResult.[[Value]].
+        Ok(realm) => realm,
+        // 4. Else, let thenRealm be the current Realm Record.
+        Err(_) => agent.current_realm_id(),
+    };
+    // 6. Return the Record { [[Job]]: job, [[Realm]]: thenRealm }.
+    Job {
+        realm: Some(then_realm),
+        inner: InnerJob::PromiseResolveThenable(PromiseResolveThenableJob {
+            promise_to_resolve,
+            thenable,
+            then,
+        }),
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct PromiseReactionJob {
+    reaction: PromiseReaction,
+    argument: Value,
+}
+impl PromiseReactionJob {
+    pub(crate) fn run(self, agent: &mut Agent) -> JsResult<()> {
+        // The following are substeps of point 1 in NewPromiseReactionJob.
+        let handler_result = match agent[self.reaction].handler {
+            PromiseReactionHandler::Empty(PromiseReactionType::Fulfill) => {
+                // d.i.1. Let handlerResult be NormalCompletion(argument).
+                Ok(self.argument)
+            }
+            PromiseReactionHandler::Empty(PromiseReactionType::Reject) => {
+                // d.ii.1. Let handlerResult be ThrowCompletion(argument).
+                Err(JsError::new(self.argument))
+            }
+            // e.1. Let handlerResult be Completion(HostCallJobCallback(handler, undefined, « argument »)).
+            // TODO: Add the HostCallJobCallback host hook. For now we're using its default
+            // implementation, which is calling the thenable, since only browsers should use a
+            // different implementation.
+            PromiseReactionHandler::JobCallback(callback) => call_function(
+                agent,
+                callback,
+                Value::Undefined,
+                Some(ArgumentsList(&[self.argument])),
+            ),
+        };
+
+        // f. If promiseCapability is undefined, then
+        let Some(promise_capability) = agent[self.reaction].capability else {
+            // i. Assert: handlerResult is not an abrupt completion.
+            handler_result.unwrap();
+            // ii. Return empty.
+            return Ok(());
+        };
+        match handler_result {
+            // h. If handlerResult is an abrupt completion, then
+            Err(err) => {
+                // i. Return ? Call(promiseCapability.[[Resolve]], undefined, « handlerResult.[[Value]] »).
+                promise_capability.reject(agent, err.value())
+            }
+            // i. Else,
+            Ok(value) => {
+                // i. Return ? Call(promiseCapability.[[Reject]], undefined, « handlerResult.[[Value]] »).
+                promise_capability.resolve(agent, value)
+            }
+        };
+        Ok(())
+    }
+}
+
+/// ### [27.2.2.1 NewPromiseReactionJob ( reaction, argument )](https://tc39.es/ecma262/#sec-newpromisereactionjob)
+pub(crate) fn new_promise_reaction_job(
+    agent: &mut Agent,
+    reaction: PromiseReaction,
+    argument: Value,
+) -> Job {
+    let handler_realm = match agent[reaction].handler {
+        // 3. If reaction.[[Handler]] is not empty, then
+        PromiseReactionHandler::JobCallback(callback) => {
+            // a. Let getHandlerRealmResult be Completion(GetFunctionRealm(reaction.[[Handler]].[[Callback]])).
+            // d. NOTE: handlerRealm is never null unless the handler is undefined. When the handler is a revoked Proxy and no ECMAScript code runs, handlerRealm is used to create error objects.
+            match get_function_realm(agent, callback) {
+                // b. If getHandlerRealmResult is a normal completion, set handlerRealm to getHandlerRealmResult.[[Value]].
+                Ok(realm) => Some(realm),
+                // c. Else, set handlerRealm to the current Realm Record.
+                Err(_) => Some(agent.current_realm_id()),
+            }
+        }
+        // 2. Let handlerRealm be null.
+        PromiseReactionHandler::Empty(_) => None,
+    };
+
+    // 4. Return the Record { [[Job]]: job, [[Realm]]: handlerRealm }.
+    Job {
+        realm: handler_realm,
+        inner: InnerJob::PromiseReaction(PromiseReactionJob { reaction, argument }),
+    }
+}

nova_vm/src/ecmascript/builtins/control_abstraction_objects/promise_objects/promise_prototype.rs
@@ -13,7 +13,10 @@ use crate::{
             },
             ArgumentsList, Behaviour, Builtin,
         },
-        execution::{agent::ExceptionType, Agent, JsResult, RealmIdentifier},
+        execution::{
+            agent::{ExceptionType, PromiseRejectionTrackerOperation},
+            Agent, JsResult, RealmIdentifier,
+        },
         types::{Function, IntoValue, String, Value, BUILTIN_STRING_MEMORY},
     },
     heap::{CreateHeapData, WellKnownSymbolIndexes},
@@ -21,6 +24,7 @@ use crate::{
 
 use super::promise_abstract_operations::{
     promise_capability_records::PromiseCapability,
+    promise_jobs::new_promise_reaction_job,
     promise_reaction_records::{
         PromiseReactionHandler, PromiseReactionRecord, PromiseReactionType,
     },
@@ -132,7 +136,8 @@ pub(crate) fn perform_promise_then(
     //     a. Let onRejectedJobCallback be empty.
     // 6. Else,
     //     a. Let onRejectedJobCallback be HostMakeJobCallback(onRejected).
-    // TODO: HostMakeJobCallback
+    // TODO: Add the HostMakeJobCallback host hook. Leaving it for later, since in implementations
+    // other than browsers, [[HostDefine]] must be EMPTY.
 
     // 7. Let fulfillReaction be the PromiseReaction Record { [[Capability]]: resultCapability, [[Type]]: fulfill, [[Handler]]: onFulfilledJobCallback }.
     let fulfill_reaction = agent.heap.create(PromiseReactionRecord {
@@ -178,30 +183,37 @@ pub(crate) fn perform_promise_then(
             };
         }
         // 10. Else if promise.[[PromiseState]] is fulfilled, then
-        PromiseState::Fulfilled { .. } => {
+        PromiseState::Fulfilled { promise_result } => {
+            let promise_result = *promise_result;
             // a. Let value be promise.[[PromiseResult]].
             // b. Let fulfillJob be NewPromiseReactionJob(fulfillReaction, value).
-            //let fulfill_job = new_promise_reaction_job(agent, fulfill_reaction, promise_result);
+            let fulfill_job = new_promise_reaction_job(agent, fulfill_reaction, promise_result);
             // c. Perform HostEnqueuePromiseJob(fulfillJob.[[Job]], fulfillJob.[[Realm]]).
-            //agent.host_hooks.enqueue_promise_job(fulfill_job);
+            agent.host_hooks.enqueue_promise_job(fulfill_job);
         }
         // 11. Else,
-        PromiseState::Rejected { is_handled, .. } => {
+        PromiseState::Rejected {
+            promise_result,
+            is_handled,
+        } => {
+            let promise_result = *promise_result;
             // a. Assert: The value of promise.[[PromiseState]] is rejected.
             // b. Let reason be promise.[[PromiseResult]].
             // c. If promise.[[PromiseIsHandled]] is false, perform HostPromiseRejectionTracker(promise, "handle").
             if !*is_handled {
-                //agent
-                //    .host_hooks
-                //    .promise_rejection_tracker(promise, PromiseRejectionTrackerOperation::Handle);
-
                 // 12. Set promise.[[PromiseIsHandled]] to true.
+                // NOTE: `is_handled` is tied to the agent's lifetime, so we need to use and drop
+                // the mutable reference before calling into the host hook.
                 *is_handled = true;
+
+                agent
+                    .host_hooks
+                    .promise_rejection_tracker(promise, PromiseRejectionTrackerOperation::Handle);
             }
             // d. Let rejectJob be NewPromiseReactionJob(rejectReaction, reason).
-            //let reject_job = new_promise_reaction_job(agent, reject_reaction, promise_result);
+            let reject_job = new_promise_reaction_job(agent, reject_reaction, promise_result);
             // e. Perform HostEnqueuePromiseJob(rejectJob.[[Job]], rejectJob.[[Realm]]).
-            //agent.host_hooks.enqueue_promise_job(reject_job);
+            agent.host_hooks.enqueue_promise_job(reject_job);
         }
     }
 }

nova_vm/src/ecmascript/builtins/promise/data.rs
@@ -4,7 +4,9 @@
 
 use crate::{
     ecmascript::{
-        builtins::control_abstraction_objects::promise_objects::promise_abstract_operations::promise_reaction_records::PromiseReaction,
+        builtins::control_abstraction_objects::promise_objects::promise_abstract_operations::{
+            promise_jobs::new_promise_reaction_job, promise_reaction_records::PromiseReaction,
+        },
         execution::Agent,
         types::{OrdinaryObject, Value},
     },
@@ -53,8 +55,19 @@ pub(crate) enum PromiseReactions {
 
 impl PromiseReactions {
     /// ### [27.2.1.8 TriggerPromiseReactions ( reactions, argument )](https://tc39.es/ecma262/#sec-triggerpromisereactions)
-    pub(crate) fn trigger(&self, _agent: &mut Agent, _argument: Value) {
-        // TODO
+    pub(crate) fn trigger(&self, agent: &mut Agent, argument: Value) {
+        match self {
+            PromiseReactions::One(reaction) => {
+                let job = new_promise_reaction_job(agent, *reaction, argument);
+                agent.host_hooks.enqueue_promise_job(job);
+            }
+            PromiseReactions::Many(vec) => {
+                for reaction in vec {
+                    let job = new_promise_reaction_job(agent, *reaction, argument);
+                    agent.host_hooks.enqueue_promise_job(job);
+                }
+            }
+        };
     }
 }
 

nova_vm/src/ecmascript/execution/agent.rs
@@ -14,7 +14,7 @@ use super::{
 use crate::{
     ecmascript::{
         abstract_operations::type_conversion::to_string,
-        builtins::error::ErrorHeapData,
+        builtins::{control_abstraction_objects::promise_objects::promise_abstract_operations::promise_jobs::{PromiseReactionJob, PromiseResolveThenableJob}, error::ErrorHeapData, promise::Promise},
         scripts_and_modules::ScriptOrModule,
         types::{Function, IntoValue, Reference, String, Symbol, Value},
     },
@@ -51,6 +51,53 @@ impl JsError {
 // #[derive(Debug)]
 // pub struct PreAllocated;
 
+pub(crate) enum InnerJob {
+    PromiseResolveThenable(PromiseResolveThenableJob),
+    PromiseReaction(PromiseReactionJob),
+}
+
+pub struct Job {
+    pub(crate) realm: Option<RealmIdentifier>,
+    pub(crate) inner: InnerJob,
+}
+
+impl Job {
+    pub fn realm(&self) -> Option<RealmIdentifier> {
+        self.realm
+    }
+
+    pub fn run(&self, agent: &mut Agent) -> JsResult<()> {
+        let mut pushed_context = false;
+        if let Some(realm) = self.realm {
+            if agent.current_realm_id() != realm {
+                agent.execution_context_stack.push(ExecutionContext {
+                    ecmascript_code: None,
+                    function: None,
+                    realm,
+                    script_or_module: None,
+                });
+                pushed_context = true;
+            }
+        };
+
+        let result = match self.inner {
+            InnerJob::PromiseResolveThenable(job) => job.run(agent),
+            InnerJob::PromiseReaction(job) => job.run(agent),
+        };
+
+        if pushed_context {
+            agent.execution_context_stack.pop();
+        }
+
+        result
+    }
+}
+
+pub enum PromiseRejectionTrackerOperation {
+    Reject,
+    Handle,
+}
+
 pub trait HostHooks: std::fmt::Debug {
     /// ### [19.2.1.2 HostEnsureCanCompileStrings ( calleeRealm )](https://tc39.es/ecma262/#sec-hostensurecancompilestrings)
     fn host_ensure_can_compile_strings(&self, _callee_realm: &mut Realm) -> JsResult<()> {
@@ -62,6 +109,18 @@ pub trait HostHooks: std::fmt::Debug {
     fn host_has_source_text_available(&self, _func: Function) -> bool {
         // The default implementation of HostHasSourceTextAvailable is to return true.
         true
+    }
+
+    /// ### [9.5.5 HostEnqueuePromiseJob ( job, realm )](https://tc39.es/ecma262/#sec-hostenqueuepromisejob)
+    fn enqueue_promise_job(&self, job: Job);
+
+    /// ### [27.2.1.9 HostPromiseRejectionTracker ( promise, operation )](https://tc39.es/ecma262/#sec-host-promise-rejection-tracker)
+    fn promise_rejection_tracker(
+        &self,
+        _promise: Promise,
+        _operation: PromiseRejectionTrackerOperation,
+    ) {
+        // The default implementation of HostPromiseRejectionTracker is to return unused.
     }
 }
 

nova_vm/src/ecmascript/execution/default_host_hooks.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use super::agent::HostHooks;
+use super::agent::{HostHooks, Job};
 
 /// A default implementation of host hooks, meant for applications that don't
 /// need an event loop or microtasks.
@@ -25,4 +25,8 @@ use super::agent::HostHooks;
 #[derive(Debug)]
 pub struct DefaultHostHooks;
 
-impl HostHooks for DefaultHostHooks {}
+impl HostHooks for DefaultHostHooks {
+    fn enqueue_promise_job(&self, _job: Job) {
+        // No-op
+    }
+}

commit f9cf07ca58b3897a6be5096ec29fa1d46bddf6bb

fix(nova_cli): The promise job queue should be a queue, not a stack
This fixes a bug introduced in #303.

nova_cli/src/main.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use std::{cell::RefCell, fmt::Debug};
+use std::{cell::RefCell, collections::VecDeque, fmt::Debug};
 
 use clap::{Parser as ClapParser, Subcommand};
 use nova_vm::ecmascript::{
@@ -47,7 +47,7 @@ enum Command {
 
 #[derive(Default)]
 struct CliHostHooks {
-    promise_job_queue: RefCell<Vec<Job>>,
+    promise_job_queue: RefCell<VecDeque<Job>>,
 }
 
 // RefCell doesn't implement Debug
@@ -61,13 +61,13 @@ impl Debug for CliHostHooks {
 
 impl CliHostHooks {
     fn pop_promise_job(&self) -> Option<Job> {
-        self.promise_job_queue.borrow_mut().pop()
+        self.promise_job_queue.borrow_mut().pop_front()
     }
 }
 
 impl HostHooks for CliHostHooks {
     fn enqueue_promise_job(&self, job: Job) {
-        self.promise_job_queue.borrow_mut().push(job);
+        self.promise_job_queue.borrow_mut().push_back(job);
     }
 }
 