de5e955b000f3b2d3a6321b2299bbbfe6d87b27c
Elias Sjögreen <elias@5monkeys.se>
2024-07-18 12:11:50 +0200

    fix: revert previous commit with an added comment

--- nova_vm/src/ecmascript/types/language/number.rs
+++ nova_vm/src/ecmascript/types/language/number.rs
@@ -287,10 +287,14 @@ impl Number {
         }
     }
 
-    pub fn is_pos_one(self) -> bool {
+    pub fn is_pos_one(self, agent: &Agent) -> bool {
+        // NOTE: Only the integer variant should ever return true, if any other
+        // variant returns true, that's a bug as it means that our variants are
+        // no longer "sound".
         match self {
+            Number::Number(n) => 1.0 == agent[n],
             Number::Integer(n) => 1i64 == n.into(),
-            _ => unreachable!(),
+            Number::Float(n) => 1.0 == n,
         }
     }
 
