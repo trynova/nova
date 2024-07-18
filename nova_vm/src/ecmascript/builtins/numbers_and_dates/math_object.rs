a2d6334d457b3b25aed4815a91f629083e7cabf1
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-07-18 14:04:02 +0000

    feat: fixed vec size

--- nova_vm/src/ecmascript/builtins/numbers_and_dates/math_object.rs
+++ nova_vm/src/ecmascript/builtins/numbers_and_dates/math_object.rs
@@ -768,7 +768,7 @@ impl MathObject {
 
     fn hypot(agent: &mut Agent, _this_value: Value, arguments: ArgumentsList) -> JsResult<Value> {
         // 1. Let coerced be a new empty List.
-        let mut coerced = vec![];
+        let mut coerced = Vec::with_capacity(arguments.len());
 
         // 2. For each element arg of args, do
         for &arg in arguments.iter() {
