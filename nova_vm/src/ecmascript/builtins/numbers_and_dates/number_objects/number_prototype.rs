f96ecb9ee1e49972a6b143e6214cd80c54946d40
marc2332 <mespinsanz@gmail.com>
2024-08-03 13:09:50 +0200

    feat: `Number.prototype.toPrecision`

--- nova_vm/src/ecmascript/builtins/numbers_and_dates/number_objects/number_prototype.rs
+++ nova_vm/src/ecmascript/builtins/numbers_and_dates/number_objects/number_prototype.rs
@@ -160,8 +160,210 @@ impl NumberPrototype {
         Self::to_string(agent, this_value, arguments)
     }
 
-    fn to_precision(_agent: &mut Agent, _this_value: Value, _: ArgumentsList) -> JsResult<Value> {
-        todo!()
+    /// ### [21.1.3.5 Number.prototype.toPrecision ( )](https://tc39.es/ecma262/#sec-number.prototype.toprecision)
+    /// Copied from Boa JS engine. Source https://github.com/boa-dev/boa/blob/6f1d7d11ce49040eafe54e5ff2da379be4d998c2/core/engine/src/builtins/number/mod.rs#L412
+    ///
+    /// Copyright (c) 2019 Jason Williams
+    fn to_precision(
+        agent: &mut Agent,
+        this_value: Value,
+        arguments: ArgumentsList,
+    ) -> JsResult<Value> {
+        // 1.
+        let x = this_number_value(agent, this_value)?;
+
+        let precision = arguments.get(0);
+        if precision.is_undefined() {
+            // 2.
+            return Self::to_string(agent, this_value, ArgumentsList(&[]));
+        }
+
+        // 3.
+        let p = to_integer_or_infinity(agent, precision)?;
+
+        if !x.is_finite(agent) {
+            // 4.
+            return Self::to_string(agent, this_value, ArgumentsList(&[]));
+        }
+
+        // 5.
+        let precision = p.into_i64(agent) as i32;
+        if precision < 1 || precision > 100 {
+            return Err(agent.throw_exception_with_static_message(ExceptionType::RangeError, ""));
+        }
+
+        // 6.
+        let mut x_f64 = x.into_f64(agent);
+
+        // 7.
+        let mut s = std::string::String::new();
+        let mut m: std::string::String;
+        let mut e: i32;
+
+        if x_f64 < 0. {
+            // 8.
+            s.push('-');
+            x_f64 = -x_f64;
+        }
+
+        if x_f64 == 0. {
+            // 9.
+            m = "0".repeat(precision as usize);
+            e = 0;
+        } else {
+            // 10.
+
+            // Due to f64 limitations, this part differs a bit from the spec,
+            // but has the same effect. It manipulates the string constructed
+            // by `format`: digits with an optional dot between two of them.
+            m = format!("{x_f64:.100}");
+
+            // a: getting an exponent
+            e = Self::flt_str_to_exp(&m);
+
+            // b: getting relevant digits only
+            if e < 0 {
+                m = m.split_off((1 - e) as usize);
+            } else if let Some(n) = m.find('.') {
+                m.remove(n);
+            }
+
+            // impl: having exactly `precision` digits in `suffix`
+            if Self::round_to_precision(&mut m, precision as usize) {
+                e += 1;
+            }
+
+            // c: switching to scientific notation
+            if e < -6 || e >= precision {
+                assert_ne!(e, 0);
+
+                // ii
+                if precision > 1 {
+                    m.insert(1, '.');
+                }
+
+                // vi
+                m.push('e');
+
+                // iii
+                if e >= precision {
+                    m.push('+');
+                }
+
+                // iv, v
+                m.push_str(&e.to_string());
+
+                return Ok(Value::from_string(agent, s + &m));
+            }
+        }
+
+        // 11
+        let e_inc = e + 1;
+        if e_inc == precision as i32 {
+            return Ok(String::from_string(agent, s + &m).into_value());
+        }
+
+        // 12
+        if e >= 0 {
+            m.insert(e_inc as usize, '.');
+        // 13
+        } else {
+            s.push('0');
+            s.push('.');
+            s.push_str(&"0".repeat(-e_inc as usize));
+        }
+
+        return Ok(String::from_string(agent, s + &m).into_value());
+    }
+
+    /// round_to_precision - used in to_precision
+    ///
+    /// This procedure has two roles:
+    /// - If there are enough or more than enough digits in the
+    ///   string to show the required precision, the number
+    ///   represented by these digits is rounded using string
+    ///   manipulation.
+    /// - Else, zeroes are appended to the string.
+    /// - Additionally, sometimes the exponent was wrongly computed and
+    ///   while up-rounding we find that we need an extra digit. When this
+    ///   happens, we return true so that the calling context can adjust
+    ///   the exponent. The string is kept at an exact length of precision.
+    ///
+    /// When this procedure returns, digits is exactly precision long.
+    ///
+    /// Copied from Boa JS engine. Source https://github.com/boa-dev/boa/blob/6f1d7d11ce49040eafe54e5ff2da379be4d998c2/core/engine/src/builtins/number/mod.rs#L351
+    ///
+    /// Copyright (c) 2019 Jason Williams
+    fn round_to_precision(digits: &mut std::string::String, precision: usize) -> bool {
+        if digits.len() > precision {
+            let to_round = digits.split_off(precision);
+            let mut digit = digits
+                .pop()
+                .expect("already checked that length is bigger than precision")
+                as u8;
+            if let Some(first) = to_round.chars().next() {
+                if first > '4' {
+                    digit += 1;
+                }
+            }
+
+            if digit as char == ':' {
+                // ':' is '9' + 1
+                // need to propagate the increment backward
+                let mut replacement = std::string::String::from("0");
+                let mut propagated = false;
+                for c in digits.chars().rev() {
+                    let d = match (c, propagated) {
+                        ('0'..='8', false) => (c as u8 + 1) as char,
+                        (_, false) => '0',
+                        (_, true) => c,
+                    };
+                    replacement.push(d);
+                    if d != '0' {
+                        propagated = true;
+                    }
+                }
+                digits.clear();
+                let replacement = if propagated {
+                    replacement.as_str()
+                } else {
+                    digits.push('1');
+                    &replacement.as_str()[1..]
+                };
+                for c in replacement.chars().rev() {
+                    digits.push(c);
+                }
+                !propagated
+            } else {
+                digits.push(digit as char);
+                false
+            }
+        } else {
+            digits.push_str(&"0".repeat(precision - digits.len()));
+            false
+        }
+    }
+
+    /// Copied from Boa JS engine. Source https://github.com/boa-dev/boa/blob/6f1d7d11ce49040eafe54e5ff2da379be4d998c2/core/engine/src/builtins/number/mod.rs#L318
+    ///
+    /// Copyright (c) 2019 Jason Williams
+    fn flt_str_to_exp(flt: &str) -> i32 {
+        let mut non_zero_encountered = false;
+        let mut dot_encountered = false;
+        for (i, c) in flt.chars().enumerate() {
+            if c == '.' {
+                if non_zero_encountered {
+                    return (i as i32) - 1;
+                }
+                dot_encountered = true;
+            } else if c != '0' {
+                if dot_encountered {
+                    return 1 - (i as i32);
+                }
+                non_zero_encountered = true;
+            }
+        }
+        (flt.len() as i32) - 1
     }
 
     fn to_string(
