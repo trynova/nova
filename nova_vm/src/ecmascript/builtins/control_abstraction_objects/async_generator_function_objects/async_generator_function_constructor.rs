dd298e987642ed9d0ac70cc1d4cdaf4f0e0b51f9
Yuto Yoshino <yossydev@yossy.dev>
2025-01-08 05:31:19 +0900

    feat(ecmascript): async generator function constructor (#516)

--- nova_vm/src/ecmascript/builtins/control_abstraction_objects/async_generator_function_objects/async_generator_function_constructor.rs
+++ nova_vm/src/ecmascript/builtins/control_abstraction_objects/async_generator_function_objects/async_generator_function_constructor.rs
@@ -2,13 +2,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use crate::ecmascript::types::IntoValue;
 use crate::engine::context::GcScope;
 use crate::{
     ecmascript::{
         builders::builtin_function_builder::BuiltinFunctionBuilder,
         builtins::{ArgumentsList, Behaviour, Builtin, BuiltinIntrinsicConstructor},
         execution::{Agent, JsResult, RealmIdentifier},
-        types::{IntoObject, Object, String, Value, BUILTIN_STRING_MEMORY},
+        fundamental_objects::function_objects::function_constructor::{
+            create_dynamic_function, DynamicFunctionKind,
+        },
+        types::{Function, IntoObject, Object, String, Value, BUILTIN_STRING_MEMORY},
     },
     heap::IntrinsicConstructorIndexes,
 };
@@ -27,13 +31,35 @@ impl BuiltinIntrinsicConstructor for AsyncGeneratorFunctionConstructor {
 
 impl AsyncGeneratorFunctionConstructor {
     fn constructor(
-        _agent: &mut Agent,
+        agent: &mut Agent,
         _this_value: Value,
-        _arguments: ArgumentsList,
-        _new_target: Option<Object>,
-        _gc: GcScope<'_, '_>,
+        arguments: ArgumentsList,
+        new_target: Option<Object>,
+        gc: GcScope<'_, '_>,
     ) -> JsResult<Value> {
-        todo!()
+        // 2. If bodyArg is not present, set bodyArg to the empty String.
+        let (parameter_args, body_arg) = if arguments.is_empty() {
+            (&[] as &[Value], String::EMPTY_STRING.into_value())
+        } else {
+            let (last, others) = arguments.split_last().unwrap();
+            (others, *last)
+        };
+        let constructor = if let Some(new_target) = new_target {
+            Function::try_from(new_target).unwrap()
+        } else {
+            agent.running_execution_context().function.unwrap()
+        };
+        // 3. Return ? CreateDynamicFunction(C, NewTarget, async, parameterArgs, bodyArg).
+        let f = create_dynamic_function(
+            agent,
+            constructor,
+            DynamicFunctionKind::AsyncGenerator,
+            parameter_args,
+            body_arg,
+            gc,
+        )?;
+
+        Ok(f.into_value())
     }
 
     pub(crate) fn create_intrinsic(agent: &mut Agent, realm: RealmIdentifier) {
