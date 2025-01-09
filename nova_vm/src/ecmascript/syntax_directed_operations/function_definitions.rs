77a5f62ffdf2ee32628d92bb3aab4f9c921f2e0b
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-01-09 21:09:48 +0200

    feat(ecmascript): Implement EvaluateAsyncGeneratorBody

--- nova_vm/src/ecmascript/syntax_directed_operations/function_definitions.rs
+++ nova_vm/src/ecmascript/syntax_directed_operations/function_definitions.rs
@@ -3,6 +3,7 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use crate::ecmascript::abstract_operations::operations_on_objects::try_define_property_or_throw;
+use crate::ecmascript::builtins::async_generator_objects::AsyncGeneratorState;
 use crate::engine::context::{GcScope, NoGcScope};
 use crate::engine::unwrap_try;
 use crate::{
@@ -421,5 +422,49 @@ pub(crate) fn evaluate_generator_body(
     });
 
     // 5. Return Completion Record { [[Type]]: return, [[Value]]: G, [[Target]]: empty }.
+    Ok(generator.into_value())
+}
+
+/// ### [15.6.2 Runtime Semantics: EvaluateAsyncGeneratorBody](https://tc39.es/ecma262/#sec-runtime-semantics-evaluateasyncgeneratorbody)
+///
+/// The syntax-directed operation EvaluateAsyncGeneratorBody takes arguments
+/// functionObject (an ECMAScript function object) and argumentsList (a List of
+/// ECMAScript language values) and returns a throw completion or a return
+/// completion.
+pub(crate) fn evaluate_async_generator_body(
+    agent: &mut Agent,
+    function_object: ECMAScriptFunction,
+    arguments_list: ArgumentsList,
+    mut gc: GcScope<'_, '_>,
+) -> JsResult<Value> {
+    let function_object = function_object.bind(gc.nogc());
+    // 1. Perform ? FunctionDeclarationInstantiation(functionObject, argumentsList).
+
+    let scoped_function_object = function_object.scope(agent, gc.nogc());
+    // 2. Let generator be ? OrdinaryCreateFromConstructor(functionObject,
+    //    "%AsyncGeneratorPrototype%", « [[AsyncGeneratorState]],
+    //    [[AsyncGeneratorContext]], [[AsyncGeneratorQueue]],
+    //    [[GeneratorBrand]] »).
+    let generator = ordinary_create_from_constructor(
+        agent,
+        function_object.into_function().unbind(),
+        ProtoIntrinsics::AsyncGenerator,
+        gc.reborrow(),
+    )?;
+    let Object::AsyncGenerator(generator) = generator else {
+        unreachable!()
+    };
+
+    // 3. Set generator.[[GeneratorBrand]] to empty.
+    // 4. Set generator.[[AsyncGeneratorState]] to suspended-start.
+    // 5. Perform AsyncGeneratorStart(generator, FunctionBody).
+    let data = CompileFunctionBodyData::new(agent, scoped_function_object.get(agent));
+    let executable = Executable::compile_function_body(agent, data, gc.nogc());
+    agent[generator].generator_state = Some(AsyncGeneratorState::Suspended {
+        vm_or_args: VmOrArguments::Arguments(arguments_list.0.into()),
+        executable,
+        execution_context: agent.running_execution_context().clone(),
+    });
+    // 6. Return ReturnCompletion(generator).
     Ok(generator.into_value())
 }
