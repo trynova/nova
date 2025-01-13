74cc6282c93e4147a0656271fd78920fce636c14
yossydev <yossydev@yossy.dev>
2025-01-13 15:35:25 +0900

    chore(ecmascript): refactor

--- nova_vm/src/ecmascript/builtins/proxy.rs
+++ nova_vm/src/ecmascript/builtins/proxy.rs
@@ -1314,7 +1314,7 @@ impl InternalMethods for Proxy {
                 target.internal_get_own_property(agent, next_key, gc.reborrow())?
             };
             //  b. If desc is not undefined and desc.[[Configurable]] is false, then
-            if desc.unwrap().configurable == Some(false) {
+            if desc.map_or(false, |d| d.configurable == Some(false)) {
                 // i. Append key to targetNonconfigurableKeys.
                 target_nonconfigurable_keys.push(key);
             } else {
