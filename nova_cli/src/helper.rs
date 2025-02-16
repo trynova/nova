124147c9dcc9aee8537fcb4391e0a9df798de25c
Andreu Botella <andreu@andreubotella.com>
2025-02-16 04:02:30 +0100

    Add an `--expose-internals` CLI flag
    The test262 runner uses the Nova CLI, but it also needs access to
    functions exposing either some of the engine's internals, or things
    that are not accessible from JS code but could be from a runtime built
    on top of the engine. An example of this is `detachArrayBuffer`, which
    is currently exposed on the global.
    
    Rather than exposing these functions on the global, this patch instead
    adds an `--expose-internals` CLI flag, which adds a `__nova__` object
    to the global which would contain those functions. So the function to
    detach an ArrayBuffer would now be accessible from
    `__nova__.detachArrayBuffer`.
    
    As part of this, this patch also adds a public
    `OrdinaryObject::create` static method that allows creating objects
    by embedders, which was not previously possible. This object is
    identical to that which would be created by evaluating `{}` in the
    current realm.

--- nova_cli/src/helper.rs
+++ nova_cli/src/helper.rs
@@ -1,7 +1,10 @@
 use nova_vm::ecmascript::{
     builtins::{create_builtin_function, ArgumentsList, Behaviour, BuiltinFunctionArgs},
     execution::{agent::ExceptionType, Agent, JsResult},
-    types::{InternalMethods, IntoValue, Object, PropertyDescriptor, PropertyKey, String, Value},
+    types::{
+        InternalMethods, IntoValue, Object, OrdinaryObject, PropertyDescriptor, PropertyKey,
+        String, Value,
+    },
 };
 use nova_vm::engine::context::GcScope;
 use oxc_diagnostics::OxcDiagnostic;
@@ -43,24 +46,6 @@ pub fn initialize_global_object(agent: &mut Agent, global: Object, mut gc: GcSco
         let file = std::fs::read_to_string(path.as_str(agent))
             .map_err(|e| agent.throw_exception(ExceptionType::Error, e.to_string(), gc.nogc()))?;
         Ok(String::from_string(agent, file, gc.nogc()).into_value())
-    }
-
-    // `detachArrayBuffer` function
-    fn detach_array_buffer(
-        agent: &mut Agent,
-        _this: Value,
-        args: ArgumentsList,
-        gc: GcScope,
-    ) -> JsResult<Value> {
-        let Value::ArrayBuffer(array_buffer) = args.get(0) else {
-            return Err(agent.throw_exception_with_static_message(
-                ExceptionType::Error,
-                "Cannot detach non ArrayBuffer argument",
-                gc.nogc(),
-            ));
-        };
-        array_buffer.detach(agent, None, gc.nogc())?;
-        Ok(Value::Undefined)
     }
 
     let function = create_builtin_function(
@@ -106,6 +91,45 @@ pub fn initialize_global_object(agent: &mut Agent, global: Object, mut gc: GcSco
             gc.reborrow(),
         )
         .unwrap();
+}
+
+pub fn initialize_global_object_with_internals(agent: &mut Agent, global: Object, mut gc: GcScope) {
+    // `detachArrayBuffer` function
+    fn detach_array_buffer(
+        agent: &mut Agent,
+        _this: Value,
+        args: ArgumentsList,
+        gc: GcScope,
+    ) -> JsResult<Value> {
+        let Value::ArrayBuffer(array_buffer) = args.get(0) else {
+            return Err(agent.throw_exception_with_static_message(
+                ExceptionType::Error,
+                "Cannot detach non ArrayBuffer argument",
+                gc.nogc(),
+            ));
+        };
+        array_buffer.detach(agent, None, gc.nogc())?;
+        Ok(Value::Undefined)
+    }
+
+    initialize_global_object(agent, global, gc.reborrow());
+
+    let nova_obj = OrdinaryObject::create(agent, gc.nogc()).scope(agent, gc.nogc());
+    let property_key = PropertyKey::from_static_str(agent, "__nova__", gc.nogc()).unbind();
+    global
+        .internal_define_own_property(
+            agent,
+            property_key,
+            PropertyDescriptor {
+                value: Some(nova_obj.get(agent).into_value()),
+                writable: Some(true),
+                enumerable: Some(false),
+                configurable: Some(true),
+                ..Default::default()
+            },
+            gc.reborrow(),
+        )
+        .unwrap();
 
     let function = create_builtin_function(
         agent,
@@ -114,7 +138,8 @@ pub fn initialize_global_object(agent: &mut Agent, global: Object, mut gc: GcSco
         gc.nogc(),
     );
     let property_key = PropertyKey::from_static_str(agent, "detachArrayBuffer", gc.nogc()).unbind();
-    global
+    nova_obj
+        .get(agent)
         .internal_define_own_property(
             agent,
             property_key,
