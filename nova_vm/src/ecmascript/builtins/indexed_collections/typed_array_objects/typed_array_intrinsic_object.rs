ba8caaecb4ff62c0e30905ab98c00cbd372c8f60
yossydev <yossydev@yossy.dev>
2025-03-27 20:55:44 +0900

    feat(ecmascript): %TypedArray%.prototype.copyWithin

--- nova_vm/src/ecmascript/builtins/indexed_collections/typed_array_objects/typed_array_intrinsic_object.rs
+++ nova_vm/src/ecmascript/builtins/indexed_collections/typed_array_objects/typed_array_intrinsic_object.rs
@@ -26,7 +26,9 @@ use crate::{
         builtins::{
             ArgumentsList, Behaviour, Builtin, BuiltinGetter, BuiltinIntrinsic,
             BuiltinIntrinsicConstructor,
-            array_buffer::{Ordering, get_value_from_buffer, is_detached_buffer},
+            array_buffer::{
+                Ordering, get_value_from_buffer, is_detached_buffer, set_value_in_buffer,
+            },
             indexed_collections::array_objects::{
                 array_iterator_objects::array_iterator::{ArrayIterator, CollectionIteratorKind},
                 array_prototype::find_via_predicate,
@@ -788,13 +790,286 @@ impl TypedArrayPrototype {
         Ok(Value::try_from(o.byte_offset(agent) as i64).unwrap())
     }
 
+    /// ### [23.2.3.6 %TypedArray%.prototype.copyWithin ( target, start [ , end ] )](https://tc39.es/ecma262/multipage/indexed-collections.html#sec-typedarray-objects)
+    /// The interpretation and use of the arguments of this method
+    /// are the same as for Array.prototype.copyWithin as defined in 23.1.3.4.
     fn copy_within<'gc>(
-        _agent: &mut Agent,
-        _this_value: Value,
-        _: ArgumentsList,
-        _gc: GcScope<'gc, '_>,
+        agent: &mut Agent,
+        this_value: Value,
+        arguments: ArgumentsList,
+        mut gc: GcScope<'gc, '_>,
     ) -> JsResult<Value<'gc>> {
-        todo!()
+        let this_value = this_value.bind(gc.nogc());
+        let target = arguments.get(0).bind(gc.nogc());
+        let start = arguments.get(1).bind(gc.nogc());
+        let end = if arguments.len() >= 3 {
+            Some(arguments.get(2).bind(gc.nogc()))
+        } else {
+            None
+        };
+        // 1. Let O be the this value.
+        let o = this_value;
+
+        let end = end.map(|e| e.scope(agent, gc.nogc()));
+        let start = start.scope(agent, gc.nogc());
+        let target = target.scope(agent, gc.nogc());
+
+        // 2. Let taRecord be ? ValidateTypedArray(O, seq-cst).
+        let mut ta_record = validate_typed_array(agent, o, Ordering::SeqCst, gc.nogc())?;
+        let o = ta_record.object;
+        let scoped_o = o.scope(agent, gc.nogc());
+        // 3. Let len be TypedArrayLength(taRecord).
+        let len = match scoped_o.get(agent) {
+            TypedArray::Int8Array(_)
+            | TypedArray::Uint8Array(_)
+            | TypedArray::Uint8ClampedArray(_) => {
+                typed_array_length::<u8>(agent, &ta_record, gc.nogc())
+            }
+            TypedArray::Int16Array(_) | TypedArray::Uint16Array(_) => {
+                typed_array_length::<u16>(agent, &ta_record, gc.nogc())
+            }
+            #[cfg(feature = "proposal-float16array")]
+            TypedArray::Float16Array(_) => typed_array_length::<f16>(agent, &ta_record, gc.nogc()),
+            TypedArray::Int32Array(_)
+            | TypedArray::Uint32Array(_)
+            | TypedArray::Float32Array(_) => {
+                typed_array_length::<u32>(agent, &ta_record, gc.nogc())
+            }
+            TypedArray::BigInt64Array(_)
+            | TypedArray::BigUint64Array(_)
+            | TypedArray::Float64Array(_) => {
+                typed_array_length::<u64>(agent, &ta_record, gc.nogc())
+            }
+        }
+        .to_i64()
+        .unwrap();
+        // 4. Let relativeTarget be ? ToIntegerOrInfinity(target).
+        let relative_target =
+            to_integer_or_infinity(agent, unsafe { target.take(agent) }, gc.reborrow())?;
+        // 5. If relativeTarget = -∞, let targetIndex be 0.
+        let target_index = if relative_target.is_neg_infinity() {
+            0
+        } else if relative_target.is_negative() {
+            // 6. Else if relativeTarget < 0, let targetIndex be max(len + relativeTarget, 0).
+            (len + relative_target.into_i64()).max(0)
+        } else {
+            // 7. Else, let targetIndex be min(relativeTarget, len).
+            relative_target.into_i64().min(len)
+        };
+        // 8. Let relativeStart be ? ToIntegerOrInfinity(start).
+        let relative_start =
+            to_integer_or_infinity(agent, unsafe { start.take(agent) }, gc.reborrow())?;
+        let start_index = if relative_start.is_neg_infinity() {
+            // 9. If relativeStart = -∞, let startIndex be 0
+            0
+        } else if relative_start.is_negative() {
+            // 10. Else if relativeStart < 0, let startIndex be max(len + relativeStart, 0).
+            (len + relative_start.into_i64()).max(0)
+        } else {
+            // 11. Else, let startIndex be min(relativeStart, len).
+            relative_start.into_i64().min(len)
+        };
+        // 12. If end is undefined, let relativeEnd be len; else let relativeEnd be ? ToIntegerOrInfinity(end).
+        let end = end.map(|e| unsafe { e.take(agent) }.bind(gc.nogc()));
+        let end_index = if end.is_none() || end.unwrap().is_undefined() {
+            len
+        } else {
+            let relative_end = to_integer_or_infinity(agent, end.unwrap().unbind(), gc.reborrow())?;
+            // 13. If relativeEnd = -∞, let endIndex be 0.
+            if relative_end.is_neg_infinity() {
+                0
+            } else if relative_end.is_negative() {
+                // 14. Else if relativeEnd < 0, let endIndex be max(len + relativeEnd, 0).
+                (len + relative_end.into_i64()).max(0)
+            } else {
+                // 15. Else, let endIndex be min(relativeEnd, len).
+                relative_end.into_i64().min(len)
+            }
+        };
+        // 16. Let count be min(endIndex - startIndex, len - targetIndex).
+        let count = (end_index - start_index).min(len - target_index);
+        // 17. If count > 0, then
+        if count > 0 {
+            // a. NOTE: The copying must be performed in a manner that preserves the bit-level encoding of the source data.
+            // b. Let buffer be O.[[ViewedArrayBuffer]].
+            let buffer = scoped_o
+                .get(agent)
+                .get_viewed_array_buffer(agent, gc.nogc());
+            // c. Set taRecord to MakeTypedArrayWithBufferWitnessRecord(O, seq-cst).
+            ta_record = make_typed_array_with_buffer_witness_record(
+                agent,
+                scoped_o.get(agent),
+                Ordering::SeqCst,
+                gc.nogc(),
+            );
+            // d. If IsTypedArrayOutOfBounds(taRecord) is true, throw a TypeError exception.
+            if match scoped_o.get(agent) {
+                TypedArray::Int8Array(_) => {
+                    is_typed_array_out_of_bounds::<i8>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::Uint8Array(_) => {
+                    is_typed_array_out_of_bounds::<u8>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::Uint8ClampedArray(_) => {
+                    is_typed_array_out_of_bounds::<U8Clamped>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::Int16Array(_) => {
+                    is_typed_array_out_of_bounds::<i16>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::Uint16Array(_) => {
+                    is_typed_array_out_of_bounds::<u16>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::Int32Array(_) => {
+                    is_typed_array_out_of_bounds::<i32>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::Uint32Array(_) => {
+                    is_typed_array_out_of_bounds::<u32>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::BigInt64Array(_) => {
+                    is_typed_array_out_of_bounds::<i64>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::BigUint64Array(_) => {
+                    is_typed_array_out_of_bounds::<u64>(agent, &ta_record, gc.nogc())
+                }
+                #[cfg(feature = "proposal-float16array")]
+                TypedArray::Float16Array(_) => {
+                    is_typed_array_out_of_bounds::<f16>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::Float32Array(_) => {
+                    is_typed_array_out_of_bounds::<f32>(agent, &ta_record, gc.nogc())
+                }
+                TypedArray::Float64Array(_) => {
+                    is_typed_array_out_of_bounds::<f64>(agent, &ta_record, gc.nogc())
+                }
+            } {
+                return Err(agent.throw_exception_with_static_message(
+                    ExceptionType::TypeError,
+                    "Callback is not callable",
+                    gc.nogc(),
+                ));
+            }
+            // e. Set len to TypedArrayLength(taRecord).
+            // f. Let elementSize be TypedArrayElementSize(O).
+            let (len, element_size) = match scoped_o.get(agent) {
+                TypedArray::Int8Array(_) => (
+                    typed_array_length::<i8>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<i8>(),
+                ),
+                TypedArray::Uint8Array(_) => (
+                    typed_array_length::<u8>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<u8>(),
+                ),
+                TypedArray::Uint8ClampedArray(_) => (
+                    typed_array_length::<U8Clamped>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<U8Clamped>(),
+                ),
+                TypedArray::Int16Array(_) => (
+                    typed_array_length::<i16>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<i16>(),
+                ),
+                TypedArray::Uint16Array(_) => (
+                    typed_array_length::<u16>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<u16>(),
+                ),
+                TypedArray::Int32Array(_) => (
+                    typed_array_length::<i32>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<i32>(),
+                ),
+                TypedArray::Uint32Array(_) => (
+                    typed_array_length::<u32>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<u32>(),
+                ),
+                TypedArray::BigInt64Array(_) => (
+                    typed_array_length::<i64>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<i64>(),
+                ),
+                TypedArray::BigUint64Array(_) => (
+                    typed_array_length::<u64>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<u64>(),
+                ),
+                #[cfg(feature = "proposal-float16array")]
+                TypedArray::Float16Array(_) => (
+                    typed_array_length::<f16>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<f16>(),
+                ),
+                TypedArray::Float32Array(_) => (
+                    typed_array_length::<f32>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<f32>(),
+                ),
+                TypedArray::Float64Array(_) => (
+                    typed_array_length::<f64>(agent, &ta_record, gc.nogc()),
+                    core::mem::size_of::<f64>(),
+                ),
+            };
+            // g. Let byteOffset be O.[[ByteOffset]].
+            let byte_offset = scoped_o.get(agent).byte_offset(agent).to_i64().unwrap();
+            let element_size = element_size.to_i64().unwrap();
+            let len = len.to_i64().unwrap();
+            // h. Let bufferByteLimit be (len × elementSize) + byteOffset.
+            let buffer_byte_limit = (len * element_size) + byte_offset;
+            // i. Let toByteIndex be (targetIndex × elementSize) + byteOffset.
+            let to_byte_index = (target_index * element_size) + byte_offset;
+            // j. Let fromByteIndex be (startIndex × elementSize) + byteOffset.
+            let from_byte_index = (start_index * element_size) + byte_offset;
+            //  k. Let countBytes be count × elementSize.
+            let mut count_bytes = count * element_size;
+            // l. If fromByteIndex < toByteIndex and toByteIndex < fromByteIndex + countBytes, then
+            let (direction, mut from_byte_index, mut to_byte_index) =
+                if from_byte_index < to_byte_index && to_byte_index < from_byte_index + count_bytes
+                {
+                    // i. Let direction be -1.
+                    // ii. Set fromByteIndex to fromByteIndex + countBytes - 1.
+                    // iii. Set toByteIndex to toByteIndex + countBytes - 1.
+                    (
+                        -1,
+                        from_byte_index + count_bytes - 1,
+                        to_byte_index + count_bytes - 1,
+                    )
+                } else {
+                    // m. Else,
+                    // i. Let direction be 1.
+                    (1, from_byte_index, to_byte_index)
+                };
+            // n. Repeat, while countBytes > 0,
+            while count_bytes > 0 {
+                // i. If fromByteIndex < bufferByteLimit and toByteIndex < bufferByteLimit, then
+                if from_byte_index < buffer_byte_limit && to_byte_index < buffer_byte_limit {
+                    //  1. Let value be GetValueFromBuffer(buffer, fromByteIndex, uint8, true, unordered).
+                    let value = get_value_from_buffer::<u8>(
+                        agent,
+                        buffer,
+                        from_byte_index.try_into().unwrap(),
+                        true,
+                        Ordering::Unordered,
+                        None,
+                        gc.nogc(),
+                    );
+                    //  2. Perform SetValueInBuffer(buffer, toByteIndex, uint8, value, true, unordered).
+                    set_value_in_buffer::<u8>(
+                        agent,
+                        buffer,
+                        to_byte_index.try_into().unwrap(),
+                        value,
+                        true,
+                        Ordering::Unordered,
+                        None,
+                    );
+                    //  3. Set fromByteIndex to fromByteIndex + direction.
+                    from_byte_index += direction;
+                    //  4. Set toByteIndex to toByteIndex + direction.
+                    to_byte_index += direction;
+                    // //  5. Set countBytes to countBytes - 1.
+                    count_bytes -= 1;
+                } else {
+                    // ii. Else,
+                    // 1. Set countBytes to 0.
+                    count_bytes = 0
+                }
+            }
+        }
+        // 18. Return O.
+        let o = scoped_o.get(agent);
+        Ok(o.into_value())
     }
 
     /// ### [23.2.3.7 %TypedArray%.prototype.entries ( )](https://tc39.es/ecma262/#sec-%typedarray%.prototype.entries)
