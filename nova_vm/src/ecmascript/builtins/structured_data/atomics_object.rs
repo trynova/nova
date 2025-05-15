7e8281ab6d22f1105e3a9bf0c63e352069974bfd
Elias Sjögreen <eliassjogreen1@gmail.com>
2025-05-15 18:37:15 +0200

    feat(ecmascript): Implement `Atomics.pause` proposal (#699)

--- nova_vm/src/ecmascript/builtins/structured_data/atomics_object.rs
+++ nova_vm/src/ecmascript/builtins/structured_data/atomics_object.rs
@@ -3,6 +3,8 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use crate::ecmascript::builtins::Behaviour;
+#[cfg(feature = "proposal-atomics-microwait")]
+use crate::ecmascript::execution::agent::ExceptionType;
 use crate::engine::context::GcScope;
 use crate::{
     ecmascript::{
@@ -120,6 +122,17 @@ impl Builtin for AtomicsObjectXor {
     const LENGTH: u8 = 3;
 
     const BEHAVIOUR: Behaviour = Behaviour::Regular(AtomicsObject::xor);
+}
+
+#[cfg(feature = "proposal-atomics-microwait")]
+struct AtomicsObjectPause;
+#[cfg(feature = "proposal-atomics-microwait")]
+impl Builtin for AtomicsObjectPause {
+    const NAME: String<'static> = BUILTIN_STRING_MEMORY.pause;
+
+    const LENGTH: u8 = 1;
+
+    const BEHAVIOUR: Behaviour = Behaviour::Regular(AtomicsObject::pause);
 }
 
 impl AtomicsObject {
@@ -240,13 +253,85 @@ impl AtomicsObject {
         Err(agent.todo("Atomics.xor", gc.into_nogc()))
     }
 
+    /// ### [1 Atomics.pause ( [ N ] )](https://tc39.es/proposal-atomics-microwait/#Atomics.pause)
+    ///
+    /// > NOTE: This method is designed for programs implementing spin-wait
+    /// > loops, such as spinlock fast paths inside of mutexes, to provide a
+    /// > hint to the CPU that it is spinning while waiting on a value. It has
+    /// > no observable behaviour other than timing.
+    /// >
+    /// > Implementations are expected to implement a pause or yield instruction
+    /// > if the best practices of the underlying architecture recommends such
+    /// > instructions in spin loops. For example, the [Intel Optimization Manual](https://www.intel.com/content/www/us/en/content-details/671488/intel-64-and-ia-32-architectures-optimization-reference-manual-volume-1.html)
+    /// > recommends the **pause** instruction.
+    ///
+    /// > NOTE: The N parameter controls how long an implementation pauses.
+    /// > Larger values result in longer waits. Implementations are encouraged
+    /// > to have an internal upper bound on the maximum amount of time paused
+    /// > on the order of tens to hundreds of nanoseconds.
+    ///
+    /// > NOTE: Due to the overhead of function calls, it is reasonable that an
+    /// > inlined call to this method in an optimizing compiler waits a
+    /// > different amount of time than a non-inlined call.
+    #[cfg(feature = "proposal-atomics-microwait")]
+    fn pause<'gc>(
+        agent: &mut Agent,
+        _this_value: Value,
+        arguments: ArgumentsList,
+        gc: GcScope<'gc, '_>,
+    ) -> JsResult<'gc, Value<'gc>> {
+        let nogc = gc.into_nogc();
+        let n = arguments.get(0);
+
+        // 1. If N is neither undefined nor an integral Number, throw a TypeError exception.
+        if !n.is_undefined() && !n.is_integer() {
+            return Err(agent.throw_exception_with_static_message(
+                ExceptionType::TypeError,
+                "Atomics.pause called with non-integral Number",
+                nogc,
+            ));
+        }
+
+        // Consider this the "internal upper bound" on the maximum amount of
+        // time paused.
+        let n = if let Value::Integer(n) = n {
+            let n = n.into_i64();
+            u16::try_from(n).unwrap_or(if n > 0 { u16::MAX } else { 1 })
+        } else {
+            1
+        };
+
+        // TODO: This should be implemented in a similar manner to `eval`
+        // where we compile calls to `Atomics.pause` as a `pause` instruction
+        // directly in the bytecode.
+
+        // 2. If the execution environment of the ECMAScript implementation supports
+        // signaling to the operating system or CPU that the current executing
+        // code is in a spin-wait loop, such as executing a pause CPU instruction,
+        // send that signal. When N is not undefined, it determines the number
+        // of times that signal is sent. The number of times the signal is sent
+        // for an integral Number N is less than or equal to the number times it
+        // is sent for N + 1 if both N and N + 1 have the same sign.
+        for _ in 0..n {
+            std::hint::spin_loop();
+        }
+
+        // 3. Return undefined.
+        Ok(Value::Undefined)
+    }
+
     pub(crate) fn create_intrinsic(agent: &mut Agent, realm: Realm<'static>) {
         let intrinsics = agent.get_realm_record_by_id(realm).intrinsics();
         let object_prototype = intrinsics.object_prototype();
         let this = intrinsics.atomics();
 
-        OrdinaryObjectBuilder::new_intrinsic_object(agent, realm, this)
-            .with_property_capacity(14)
+        let mut property_capacity = 14;
+        if cfg!(feature = "proposal-atomics-microwait") {
+            property_capacity += 1;
+        }
+
+        let builder = OrdinaryObjectBuilder::new_intrinsic_object(agent, realm, this)
+            .with_property_capacity(property_capacity)
             .with_prototype(object_prototype)
             .with_builtin_function_property::<AtomicsObjectAdd>()
             .with_builtin_function_property::<AtomicsObjectAnd>()
@@ -268,7 +353,11 @@ impl AtomicsObject {
                     .with_enumerable(false)
                     .with_configurable(true)
                     .build()
-            })
-            .build();
+            });
+
+        #[cfg(feature = "proposal-atomics-microwait")]
+        let builder = builder.with_builtin_function_property::<AtomicsObjectPause>();
+
+        builder.build();
     }
 }
