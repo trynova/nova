5e2614289f1172edc5dde772397a9585fb721429
Elias Sjögreen <eliassjogreen1@gmail.com>
2025-05-15 15:28:12 +0200

    fix: Lower upper bound to avoid freezes

--- nova_vm/src/ecmascript/builtins/structured_data/atomics_object.rs
+++ nova_vm/src/ecmascript/builtins/structured_data/atomics_object.rs
@@ -294,7 +294,7 @@ impl AtomicsObject {
 
         // Consider this the "internal upper bound" on the maximum amount of
         // time paused.
-        let n = n.to_uint32(agent, gc).unwrap_or(1);
+        let n = n.to_uint16(agent, gc).unwrap_or(1);
 
         // TODO: This should be implemented in a similar manner to `eval`
         // where we compile calls to `Atomics.pause` as a `pause` instruction
