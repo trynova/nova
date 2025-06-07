2f09304d915a3157e73ffc4a7f48bbd34aad3844
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-06-07 12:13:08 +0300

    feat(ecmascript): ExportDeclaration (#754)

---
+++ nova_vm/src/engine/bytecode/bytecode_compiler/exports.rs
@@ -0,0 +1,124 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! ## [16.2.3.7 Runtime Semantics: Evaluation](https://tc39.es/ecma262/#sec-exports-runtime-semantics-evaluation)
+
+use oxc_ast::ast;
+
+use crate::{
+    ecmascript::types::BUILTIN_STRING_MEMORY,
+    engine::{
+        Instruction, NamedEvaluationParameter,
+        bytecode::bytecode_compiler::is_anonymous_function_definition, is_reference,
+    },
+};
+
+use super::CompileEvaluation;
+
+impl<'s> CompileEvaluation<'s> for ast::ExportAllDeclaration<'s> {
+    /// ### ExportDeclaration :
+    /// ```text
+    /// export ExportFromClause FromClause WithClause_opt ;
+    /// ```
+    fn compile(&'s self, _: &mut super::CompileContext<'_, 's, '_, '_>) {
+        // 1. Return empty.
+    }
+}
+
+impl<'s> CompileEvaluation<'s> for ast::ExportDefaultDeclaration<'s> {
+    /// ### ExportDeclaration :
+    /// ```text
+    /// export default HoistableDeclaration
+    /// export default ClassDeclaration
+    /// export default AssignmentExpression ;
+    /// ```
+    fn compile(&'s self, ctx: &mut super::CompileContext<'_, 's, '_, '_>) {
+        match &self.declaration {
+            //  ExportDeclaration : export default HoistableDeclaration
+            // 1. Return ? Evaluation of HoistableDeclaration.
+            ast::ExportDefaultDeclarationKind::FunctionDeclaration(decl) => decl.compile(ctx),
+            // ExportDeclaration : export default ClassDeclaration
+            ast::ExportDefaultDeclarationKind::ClassDeclaration(decl) => {
+                // 1. Let value be ? BindingClassDeclarationEvaluation of ClassDeclaration.
+                ctx.add_instruction_with_constant(
+                    Instruction::StoreConstant,
+                    BUILTIN_STRING_MEMORY.default,
+                );
+                ctx.name_identifier = Some(NamedEvaluationParameter::Result);
+                decl.compile(ctx);
+                // 2. Let className be the sole element of the BoundNames of ClassDeclaration.
+                // 3. If className is "*default*", then
+                // a. Let env be the running execution context's LexicalEnvironment.
+                // b. Perform ? InitializeBoundName("*default*", value, env).
+                ctx.add_instruction_with_identifier(
+                    Instruction::ResolveBinding,
+                    BUILTIN_STRING_MEMORY._default_,
+                );
+                ctx.add_instruction(Instruction::InitializeReferencedBinding);
+                // 4. Return empty.
+            }
+            ast::ExportDefaultDeclarationKind::TSInterfaceDeclaration(_) => unreachable!(),
+            _ => {
+                // ExportDeclaration : export default AssignmentExpression ;
+                let expr = self.declaration.as_expression().unwrap();
+                // 1. If IsAnonymousFunctionDefinition(AssignmentExpression) is
+                //    true, then
+                if is_anonymous_function_definition(expr) {
+                    // a. Let value be ? NamedEvaluation of
+                    //    AssignmentExpression with argument "default".
+                    ctx.name_identifier = Some(NamedEvaluationParameter::Result);
+                    ctx.add_instruction_with_constant(
+                        Instruction::StoreConstant,
+                        BUILTIN_STRING_MEMORY.default,
+                    );
+                }
+                // 2. Else,
+                // a. Let rhs be ? Evaluation of AssignmentExpression.
+                expr.compile(ctx);
+                // b. Let value be ? GetValue(rhs).
+                if is_reference(expr) {
+                    ctx.add_instruction(Instruction::GetValue);
+                }
+                // 3. Let env be the running execution context's LexicalEnvironment.
+                // 4. Perform ? InitializeBoundName("*default*", value, env).
+                ctx.add_instruction_with_identifier(
+                    Instruction::ResolveBinding,
+                    BUILTIN_STRING_MEMORY._default_,
+                );
+                ctx.add_instruction(Instruction::InitializeReferencedBinding);
+                // 5. Return empty.
+            }
+        }
+    }
+}
+
+impl<'s> CompileEvaluation<'s> for ast::ExportNamedDeclaration<'s> {
+    /// ### ExportDeclaration :
+    /// ```text
+    /// export NamedExports ;
+    /// export VariableStatement
+    /// export Declaration
+    /// ```
+    fn compile(&'s self, ctx: &mut super::CompileContext<'_, 's, '_, '_>) {
+        let Some(decl) = &self.declaration else {
+            // export NamedExports ;
+            // 1. Return empty.
+            return;
+        };
+        match decl {
+            // export VariableStatement
+            // 1. Return ? Evaluation of VariableStatement.
+            ast::Declaration::VariableDeclaration(decl) => decl.compile(ctx),
+            // ExportDeclaration : export Declaration
+            // 1. Return ? Evaluation of Declaration.
+            ast::Declaration::FunctionDeclaration(decl) => decl.compile(ctx),
+            ast::Declaration::ClassDeclaration(decl) => decl.compile(ctx),
+            ast::Declaration::TSTypeAliasDeclaration(_)
+            | ast::Declaration::TSInterfaceDeclaration(_)
+            | ast::Declaration::TSEnumDeclaration(_)
+            | ast::Declaration::TSModuleDeclaration(_)
+            | ast::Declaration::TSImportEqualsDeclaration(_) => unreachable!(),
+        }
+    }
+}
