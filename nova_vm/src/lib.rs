fa468a815d2f8bbb252ec2642fcf8c64ca8ffcd1
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-09-12 11:27:06 +0300

    try: remove usdt entirely

--- nova_vm/src/lib.rs
+++ nova_vm/src/lib.rs
@@ -11,33 +11,3 @@ pub mod heap;
 pub use engine::small_integer::SmallInteger;
 use heap::Heap;
 pub use small_string::SmallString;
-
-/// DTrace / SystemTap USDT probes in Nova VM.
-#[usdt::provider(provider = "nova_vm")]
-mod ndt {
-    // // Start probes in alphabetical order.
-    // fn builtin_call_start(name: &str, id: u64) {}
-    // fn builtin_constructor_start(name: &str, id: u64) {}
-    // fn eval_evaluation_start(id: u64) {}
-    // fn gc_start() {}
-    // fn javascript_call_start(name: &str, id: u64) {}
-    // fn javascript_constructor_start(name: &str, id: u64) {}
-    // fn job_evaluation_start(id: u64) {}
-    // fn module_evaluation_start(id: u64) {}
-    // fn script_evaluation_start(id: u64) {}
-
-    // // Done probes in alphabetical order.
-    // fn builtin_call_done(id: u64) {}
-    // fn builtin_constructor_done(id: u64) {}
-    // fn eval_evaluation_done(id: u64) {}
-    // fn gc_done() {}
-    // fn javascript_constructor_done(id: u64) {}
-    // fn javascript_call_done(id: u64) {}
-    // fn job_evaluation_done(id: u64) {}
-    // fn module_evaluation_done(id: u64) {}
-    // fn script_evaluation_done(id: u64) {}
-}
-
-// Expose the USDT probe registering function. In Linux this is a no-op but it
-// is required on illumos and OS X for DTrace to work automatically.
-pub use usdt::register_probes;
