acfc097c2adb7cc0b6d3e224d4f2bc0743716419
Felipe Armoni <felipe.armoni@gmail.com>
2025-10-14 09:27:46 -0300

    Delete Promise All and Promise All Settled records

--- nova_vm/src/ecmascript/builtins/control_abstraction_objects/promise_objects/promise_abstract_operations.rs
+++ nova_vm/src/ecmascript/builtins/control_abstraction_objects/promise_objects/promise_abstract_operations.rs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-pub(crate) mod promise_all_record;
-pub(crate) mod promise_all_settled_record;
 pub mod promise_capability_records;
 pub(crate) mod promise_finally_functions;
 pub(crate) mod promise_group_record;
