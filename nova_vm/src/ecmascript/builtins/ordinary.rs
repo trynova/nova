e1158f561b2cf3061a702491a0119958e6261cb2
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-10-19 11:24:03 +0300

    fix errors after rebase

--- nova_vm/src/ecmascript/builtins/ordinary.rs
+++ nova_vm/src/ecmascript/builtins/ordinary.rs
@@ -6,13 +6,17 @@ pub(crate) mod caches;
 pub mod shape;
 
 use std::{
-    collections::{hash_map::Entry, TryReserveError}, ops::ControlFlow, time::Instant, vec
+    collections::{TryReserveError, hash_map::Entry},
+    ops::ControlFlow,
+    vec,
 };
 
 use caches::{CacheToPopulate, Caches, PropertyLookupCache, PropertyOffset};
 
 #[cfg(feature = "shared-array-buffer")]
 use crate::ecmascript::builtins::data_view::data::SharedDataViewRecord;
+#[cfg(feature = "temporal")]
+use crate::ecmascript::builtins::temporal::instant::data::InstantHeapData;
 use crate::{
     ecmascript::{
         abstract_operations::operations_on_objects::{
@@ -28,7 +32,9 @@ use crate::{
         },
     },
     engine::{
-        context::{Bindable, GcScope, NoGcScope}, rootable::Scopable, Scoped
+        Scoped,
+        context::{Bindable, GcScope, NoGcScope},
+        rootable::Scopable,
     },
     heap::element_array::{ElementStorageRef, PropertyStorageRef},
 };
@@ -1677,10 +1683,9 @@ pub(crate) fn ordinary_object_create_with_intrinsics<'a>(
             .create(ErrorHeapData::new(ExceptionType::SyntaxError, None, None))
             .into_object(),
         #[cfg(feature = "temporal")]
-        ProtoIntrinsics::TemporalInstant => agent
-            .heap
-            .create(InstantHeapData::default())
-            .into_object(),
+        ProtoIntrinsics::TemporalInstant => {
+            agent.heap.create(InstantHeapData::default()).into_object()
+        }
         ProtoIntrinsics::TypeError => agent
             .heap
             .create(ErrorHeapData::new(ExceptionType::TypeError, None, None))
@@ -2067,6 +2072,8 @@ fn get_intrinsic_constructor<'a>(
         ProtoIntrinsics::WeakRef => Some(intrinsics.weak_ref().into_function()),
         #[cfg(feature = "weak-refs")]
         ProtoIntrinsics::WeakSet => Some(intrinsics.weak_set().into_function()),
+        #[cfg(feature = "temporal")]
+        ProtoIntrinsics::TemporalInstant => Some(intrinsics.temporal_instant().into_function()),
     }
 }
 
