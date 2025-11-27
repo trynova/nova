f7eba4da258cb27de7fc5ae98b812d1d2d802fc7
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-11-27 12:04:40 +0200

    fix: finish Temporal.Instant.prototype.toString impl

--- nova_vm/src/ecmascript/builtins/temporal/duration/duration_prototype.rs
+++ nova_vm/src/ecmascript/builtins/temporal/duration/duration_prototype.rs
@@ -12,12 +12,12 @@ pub(crate) struct TemporalDurationPrototype;
 impl TemporalDurationPrototype {
     pub fn create_intrinsic(agent: &mut Agent, realm: Realm<'static>, _: NoGcScope) {
         let intrinsics = agent.get_realm_record_by_id(realm).intrinsics();
-        let this = intrinsics.temporal_instant_prototype();
+        let this = intrinsics.temporal_duration_prototype();
         let object_prototype = intrinsics.object_prototype();
         let duration_constructor = intrinsics.temporal_duration();
 
         OrdinaryObjectBuilder::new_intrinsic_object(agent, realm, this)
-            .with_property_capacity(15)
+            .with_property_capacity(2)
             .with_prototype(object_prototype)
             .with_constructor_property(duration_constructor)
             .with_property(|builder| {
