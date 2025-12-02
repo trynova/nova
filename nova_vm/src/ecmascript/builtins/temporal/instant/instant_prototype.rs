c74f2d1e2aae9a650072a6ee764d748ec66fe44a
SebastianJM <sebastianjacmatt@gmail.com>
2025-12-02 15:11:56 +0100

    to_local_string, to_json

--- nova_vm/src/ecmascript/builtins/temporal/instant/instant_prototype.rs
+++ nova_vm/src/ecmascript/builtins/temporal/instant/instant_prototype.rs
@@ -516,23 +516,53 @@ impl TemporalInstantPrototype {
     }
 
     /// ### [8.3.12 Temporal.Instant.prototype.toLocaleString ( [ locales [ , options ] ] )](https://tc39.es/proposal-temporal/#sec-temporal.instant.prototype.tolocalestring)
+    /// An ECMAScript implementation that includes the ECMA-402 Internationalization API 
+    /// must implement this method as specified in the ECMA-402 specification. 
+    /// If an ECMAScript implementation does not include the ECMA-402 API the following specification of this method is used.
+    ///
+    /// The meanings of the optional parameters to this method are defined in the ECMA-402 specification; 
+    /// implementations that do not include ECMA-402 support must not use those parameter positions for anything else.
     fn to_locale_string<'gc>(
-        _agent: &mut Agent,
-        _this_value: Value,
+        agent: &mut Agent,
+        this_value: Value,
         _args: ArgumentsList,
-        mut _gc: GcScope<'gc, '_>,
+        gc: GcScope<'gc, '_>,
     ) -> JsResult<'gc, Value<'gc>> {
-        unimplemented!()
+        // 1. Let instant be the this value.
+        let value = this_value.bind(gc.nogc());
+        // 2. Perform ? RequireInternalSlot(instant, [[InitializedTemporalInstant]]).
+        let instant = require_internal_slot_temporal_instant(agent, value, gc.nogc()).bind(gc.nogc());
+        // 3. Return TemporalInstantToString(instant, undefined, AUTO).
+        let options: ToStringRoundingOptions = ToStringRoundingOptions::default(); // defaults Precision to Auto
+        match instant.unbind()?
+            .inner_instant(agent)
+            .to_ixdtf_string(Some(TimeZone::utc()), options)
+        {
+            Ok(string) => Ok(Value::from_string(agent, string, gc.into_nogc())),
+            Err(err) => Err(temporal_err_to_js_err(agent, err, gc.into_nogc())),
+        }
     }
 
     /// ###[8.3.13 Temporal.Instant.prototype.toJSON ( )](https://tc39.es/proposal-temporal/#sec-temporal.instant.prototype.tojson)
     fn to_json<'gc>(
-        _agent: &mut Agent,
-        _this_value: Value,
+        agent: &mut Agent,
+        this_value: Value,
         _args: ArgumentsList,
-        mut _gc: GcScope<'gc, '_>,
+        gc: GcScope<'gc, '_>,
     ) -> JsResult<'gc, Value<'gc>> {
-        unimplemented!()
+        // 1. Let instant be the this value.
+        let value = this_value.bind(gc.nogc());
+        // 2. Perform ? RequireInternalSlot(instant, [[InitializedTemporalInstant]]).
+        let instant = require_internal_slot_temporal_instant(agent, value.unbind(), gc.nogc()).bind(gc.nogc());
+        // 3. Return TemporalInstantToString(instant, undefined, AUTO).
+        let options: ToStringRoundingOptions = ToStringRoundingOptions::default();
+        match instant.unbind()?
+            .inner_instant(agent)
+            .to_ixdtf_string(Some(TimeZone::utc()), options)
+        {
+            Ok(string) => Ok(Value::from_string(agent, string, gc.into_nogc())),
+            Err(err) => Err(temporal_err_to_js_err(agent, err, gc.into_nogc())),
+        }
     }
 
     /// ###[8.3.14 Temporal.Instant.prototype.valueOf ( )](https://tc39.es/proposal-temporal/#sec-temporal.instant.prototype.valueof)
@@ -560,11 +590,18 @@ impl TemporalInstantPrototype {
 
     // [8.3.15 Temporal.Instant.prototype.toZonedDateTimeISO ( timeZone )](https://tc39.es/proposal-temporal/#sec-temporal.instant.prototype.tozoneddatetimeiso)
     fn to_zoned_date_time_iso<'gc>(
-        _agent: &mut Agent,
-        _this_value: Value,
-        _args: ArgumentsList,
-        mut _gc: GcScope<'gc, '_>,
+        agent: &mut Agent,
+        this_value: Value,
+        args: ArgumentsList,
+        mut gc: GcScope<'gc, '_>,
     ) -> JsResult<'gc, Value<'gc>> {
+        // 1. Let instant be the this value.
+        let value = this_value.bind(gc.nogc());
+        // 2. Perform ? RequireInternalSlot(instant, [[InitializedTemporalInstant]]).
+        let instant = require_internal_slot_temporal_instant(agent, value.unbind(), gc.nogc()).bind(gc.nogc());
+        // 3. Set timeZone to ? ToTemporalTimeZoneIdentifier(timeZone).
+        let time_zone = Some(TimeZone::utc());
+        // 4. Return ! CreateTemporalZonedDateTime(instant.[[EpochNanoseconds]], timeZone, "iso8601").
         unimplemented!()
     }
 
