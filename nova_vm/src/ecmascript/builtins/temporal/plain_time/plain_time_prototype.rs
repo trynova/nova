52ca3442655701c3ab30c11840efd1f1070a2e2a
jesperkha <hammerjesp@gmail.com>
2026-02-24 14:00:02 +0100

    feat(ecmascript): get PlainTime.prototype.hour

--- nova_vm/src/ecmascript/builtins/temporal/plain_time/plain_time_prototype.rs
+++ nova_vm/src/ecmascript/builtins/temporal/plain_time/plain_time_prototype.rs
@@ -23,6 +23,15 @@ impl Builtin for TemporalPlainTimePrototypeGetMinute {
 }
 impl BuiltinGetter for TemporalPlainTimePrototypeGetMinute {}
 
+struct TemporalPlainTimePrototypeGetHour;
+impl Builtin for TemporalPlainTimePrototypeGetHour {
+    const NAME: String<'static> = BUILTIN_STRING_MEMORY.get_hour;
+    const KEY: Option<PropertyKey<'static>> = Some(BUILTIN_STRING_MEMORY.hour.to_property_key());
+    const LENGTH: u8 = 0;
+    const BEHAVIOUR: Behaviour = Behaviour::Regular(TemporalPlainTimePrototype::get_hour);
+}
+impl BuiltinGetter for TemporalPlainTimePrototypeGetHour {}
+
 impl TemporalPlainTimePrototype {
     /// ### [4.3.4 get Temporal.PlainTime.prototype.minute](https://tc39.es/proposal-temporal/#sec-get-temporal.plaintime.prototype.minute)
     pub(crate) fn get_minute<'gc>(
@@ -40,6 +49,22 @@ impl TemporalPlainTimePrototype {
         Ok(value.into())
     }
 
+    // ### [4.3.3 get Temporal.PlainTime.prototype.hour](https://tc39.es/proposal-temporal/#sec-get-temporal.plaintime.prototype.hour)
+    pub(crate) fn get_hour<'gc>(
+        agent: &mut Agent,
+        this_value: Value,
+        _: ArgumentsList,
+        gc: GcScope<'gc, '_>,
+    ) -> JsResult<'gc, Value<'gc>> {
+        let gc = gc.into_nogc();
+        // 1. Let plainTime be the this value.
+        // 2. Perform ? RequireInternalSlot(plainTime, [[InitializedTemporalTime]]).
+        let plain_time = require_internal_slot_temporal_plain_time(agent, this_value, gc)?;
+        // 3. Return 𝔽(plainTime.[[Time]].[[Hour]]).
+        let value = plain_time.inner_plain_time(agent).hour();
+        Ok(value.into())
+    }
+
     pub(crate) fn create_intrinsic(agent: &mut Agent, realm: Realm<'static>, _: NoGcScope) {
         let intrinsics = agent.get_realm_record_by_id(realm).intrinsics();
         let this = intrinsics.temporal_plain_time_prototype();
@@ -47,10 +72,11 @@ impl TemporalPlainTimePrototype {
         let plain_time_constructor = intrinsics.temporal_plain_time();
 
         OrdinaryObjectBuilder::new_intrinsic_object(agent, realm, this)
-            .with_property_capacity(3)
+            .with_property_capacity(4)
             .with_prototype(object_prototype)
             .with_constructor_property(plain_time_constructor)
             .with_builtin_function_getter_property::<TemporalPlainTimePrototypeGetMinute>()
+            .with_builtin_function_getter_property::<TemporalPlainTimePrototypeGetHour>()
             .with_property(|builder| {
                 builder
                     .with_key(WellKnownSymbols::ToStringTag.into())
