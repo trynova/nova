a87f57c28e2d0fa9c9304b8d6e7fa2a504928b9c
Zeno Elio Leonardi <145581496+0Zeno@users.noreply.github.com>
2026-03-05 18:34:48 +0100

    feat(ecmascript): get PlainTime.prototype.nanosecond/microsecond/millisecond/hour (#966)
    Co-authored-by: jesperkha <hammerjesp@gmail.com>

--- nova_vm/src/ecmascript/builtins/temporal/plain_time/plain_time_prototype.rs
+++ nova_vm/src/ecmascript/builtins/temporal/plain_time/plain_time_prototype.rs
@@ -14,6 +14,15 @@ use crate::{
 
 pub(crate) struct TemporalPlainTimePrototype;
 
+struct TemporalPlainTimePrototypeGetHour;
+impl Builtin for TemporalPlainTimePrototypeGetHour {
+    const NAME: String<'static> = BUILTIN_STRING_MEMORY.get_hour;
+    const KEY: Option<PropertyKey<'static>> = Some(BUILTIN_STRING_MEMORY.hour.to_property_key());
+    const LENGTH: u8 = 0;
+    const BEHAVIOUR: Behaviour = Behaviour::Regular(TemporalPlainTimePrototype::get_hour);
+}
+impl BuiltinGetter for TemporalPlainTimePrototypeGetHour {}
+
 struct TemporalPlainTimePrototypeGetMinute;
 impl Builtin for TemporalPlainTimePrototypeGetMinute {
     const NAME: String<'static> = BUILTIN_STRING_MEMORY.get_minute;
@@ -31,6 +40,38 @@ impl Builtin for TemporalPlainTimePrototypeGetSecond {
     const BEHAVIOUR: Behaviour = Behaviour::Regular(TemporalPlainTimePrototype::get_second);
 }
 impl BuiltinGetter for TemporalPlainTimePrototypeGetSecond {}
+
+struct TemporalPlainTimePrototypeGetMillisecond;
+impl Builtin for TemporalPlainTimePrototypeGetMillisecond {
+    const NAME: String<'static> = BUILTIN_STRING_MEMORY.get_millisecond;
+    const KEY: Option<PropertyKey<'static>> =
+        Some(BUILTIN_STRING_MEMORY.millisecond.to_property_key());
+    const LENGTH: u8 = 0;
+    const BEHAVIOUR: Behaviour = Behaviour::Regular(TemporalPlainTimePrototype::get_millisecond);
+}
+impl BuiltinGetter for TemporalPlainTimePrototypeGetMillisecond {}
+struct TemporalPlainTimePrototypeGetMicrosecond;
+impl Builtin for TemporalPlainTimePrototypeGetMicrosecond {
+    const NAME: String<'static> = BUILTIN_STRING_MEMORY.get_microsecond;
+    const KEY: Option<PropertyKey<'static>> =
+        Some(BUILTIN_STRING_MEMORY.microsecond.to_property_key());
+    const LENGTH: u8 = 0;
+    const BEHAVIOUR: Behaviour = Behaviour::Regular(TemporalPlainTimePrototype::get_microsecond);
+}
+impl BuiltinGetter for TemporalPlainTimePrototypeGetMicrosecond {}
+
+struct TemporalPlainTimePrototypeGetNanosecond;
+impl Builtin for TemporalPlainTimePrototypeGetNanosecond {
+    const NAME: String<'static> = BUILTIN_STRING_MEMORY.get_nanosecond;
+    const KEY: Option<PropertyKey<'static>> =
+        Some(BUILTIN_STRING_MEMORY.nanosecond.to_property_key());
+    const LENGTH: u8 = 0;
+    const BEHAVIOUR: Behaviour = Behaviour::Regular(TemporalPlainTimePrototype::get_nanosecond);
+    const WRITABLE: bool = true;
+    const ENUMERABLE: bool = false;
+    const CONFIGURABLE: bool = true;
+}
+impl BuiltinGetter for TemporalPlainTimePrototypeGetNanosecond {}
 
 impl TemporalPlainTimePrototype {
     /// ### [4.3.4 get Temporal.PlainTime.prototype.minute](https://tc39.es/proposal-temporal/#sec-get-temporal.plaintime.prototype.minute)
@@ -64,6 +105,70 @@ impl TemporalPlainTimePrototype {
         Ok(value.into())
     }
 
+    /// ### [4.3.6 get Temporal.PlainTime.prototype.millisecond](https://tc39.es/proposal-temporal/#sec-get-temporal.plaintime.prototype.millisecond)
+    pub(crate) fn get_millisecond<'gc>(
+        agent: &mut Agent,
+        this_value: Value,
+        _: ArgumentsList,
+        gc: GcScope<'gc, '_>,
+    ) -> JsResult<'gc, Value<'gc>> {
+        let gc = gc.into_nogc();
+        // 1. Let plainTime be the this value.
+        // 2. Perform ? RequireInternalSlot(plainTime, [[InitializedTemporalTime]]).
+        let plain_time = require_internal_slot_temporal_plain_time(agent, this_value, gc)?;
+        // 3. Return 𝔽(plainTime.[[Time]].[[Millisecond]]).
+        let value = plain_time.inner_plain_time(agent).millisecond();
+        Ok(value.into())
+    }
+
+    /// ### [4.3.8 get Temporal.PlainTime.prototype.nanosecond](https://tc39.es/proposal-temporal/#sec-get-temporal.plaintime.prototype.nanosecond)
+    pub(crate) fn get_nanosecond<'gc>(
+        agent: &mut Agent,
+        this_value: Value,
+        _: ArgumentsList,
+        gc: GcScope<'gc, '_>,
+    ) -> JsResult<'gc, Value<'gc>> {
+        let gc = gc.into_nogc();
+        // 1. Let plainTime be the this value.
+        // 2. Perform ? RequireInternalSlot(plainTime, [[InitializedTemporalTime]]).
+        let plain_time = require_internal_slot_temporal_plain_time(agent, this_value, gc)?;
+        // 3. Return 𝔽(plainTime.[[Time]].[[Nanosecond]]).
+        let value = plain_time.inner_plain_time(agent).nanosecond();
+        Ok(value.into())
+    }
+
+    // ### [4.3.3 get Temporal.PlainTime.prototype.hour](https://tc39.es/proposal-temporal/#sec-get-temporal.plaintime.prototype.hour)
+    pub(crate) fn get_hour<'gc>(
+        agent: &mut Agent,
+        this_value: Value,
+        _: ArgumentsList,
+        gc: GcScope<'gc, '_>,
+    ) -> JsResult<'gc, Value<'gc>> {
+        let gc = gc.into_nogc();
+        // 1. Let plainTime be the this value.
+        // 2. Perform ? RequireInternalSlot(plainTime, [[InitializedTemporalTime]]).
+        let plain_time = require_internal_slot_temporal_plain_time(agent, this_value, gc)?;
+        // 3. Return 𝔽(plainTime.[[Time]].[[Hour]]).
+        let value = plain_time.inner_plain_time(agent).hour();
+        Ok(value.into())
+    }
+
+    /// ### [4.3.4 get Temporal.PlainTime.prototype.microsecond](https://tc39.es/proposal-temporal/#sec-get-temporal.plaintime.prototype.microsecond)
+    pub(crate) fn get_microsecond<'gc>(
+        agent: &mut Agent,
+        this_value: Value,
+        _: ArgumentsList,
+        gc: GcScope<'gc, '_>,
+    ) -> JsResult<'gc, Value<'gc>> {
+        let gc = gc.into_nogc();
+        // 1. Let plainTime be the this value.
+        // 2. Perform ? RequireInternalSlot(plainTime, [[InitializedTemporalTime]]).
+        let plain_time = require_internal_slot_temporal_plain_time(agent, this_value, gc)?;
+        // 3. Return 𝔽(plainTime.[[Time]].[[Microsecond]]).
+        let value = plain_time.inner_plain_time(agent).microsecond();
+        Ok(value.into())
+    }
+
     pub(crate) fn create_intrinsic(agent: &mut Agent, realm: Realm<'static>, _: NoGcScope) {
         let intrinsics = agent.get_realm_record_by_id(realm).intrinsics();
         let this = intrinsics.temporal_plain_time_prototype();
@@ -71,11 +176,15 @@ impl TemporalPlainTimePrototype {
         let plain_time_constructor = intrinsics.temporal_plain_time();
 
         OrdinaryObjectBuilder::new_intrinsic_object(agent, realm, this)
-            .with_property_capacity(4)
+            .with_property_capacity(8)
             .with_prototype(object_prototype)
             .with_constructor_property(plain_time_constructor)
+            .with_builtin_function_getter_property::<TemporalPlainTimePrototypeGetHour>()
             .with_builtin_function_getter_property::<TemporalPlainTimePrototypeGetMinute>()
             .with_builtin_function_getter_property::<TemporalPlainTimePrototypeGetSecond>()
+            .with_builtin_function_getter_property::<TemporalPlainTimePrototypeGetMicrosecond>()
+            .with_builtin_function_getter_property::<TemporalPlainTimePrototypeGetNanosecond>()
+            .with_builtin_function_getter_property::<TemporalPlainTimePrototypeGetMillisecond>()
             .with_property(|builder| {
                 builder
                     .with_key(WellKnownSymbols::ToStringTag.into())
