602cd7d3515c034676551dcff389d7e8652ce7e0
jesperkha <hammerjesp@gmail.com>
2026-04-19 15:38:45 +0200

    Remove unused import

--- nova_vm/src/ecmascript/builtins/temporal/plain_time/plain_time_prototype.rs
+++ nova_vm/src/ecmascript/builtins/temporal/plain_time/plain_time_prototype.rs
@@ -2,10 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use temporal_rs::{
-    PlainTime,
-    options::{RoundingMode, ToStringRoundingOptions, Unit},
-};
+use temporal_rs::options::{RoundingMode, ToStringRoundingOptions, Unit};
 
 use crate::{
     ecmascript::{
